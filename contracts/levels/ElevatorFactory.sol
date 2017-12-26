01d79873424e5eaf7117b8bbf51d1a4455315d09
Martín Triay <martriay@gmail.com>
2017-12-26 15:33:37 -0300

    Elevator level + Factory

---
+++ contracts/levels/ElevatorFactory.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.18;
+
+import './base/Level.sol';
+import './Elevator.sol';
+
+contract ElevatorFactory is Level {
+
+  function createInstance(address _player) public payable returns (address) {
+    _player;
+    Elevator instance = new Elevator();
+    return instance;
+  }
+
+  function validateInstance(address _instance, address _player) public constant returns (bool) {
+    Elevator elevator = Elevator(_instance);
+    return elevator.top();
+  }
+}
