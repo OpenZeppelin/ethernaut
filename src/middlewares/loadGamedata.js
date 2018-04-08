912859c6c8e51db5e7a37145eb7794a7b7a915e7
Alejandro Santander <Palebluedot@gmail.com>
2018-04-07 23:02:21 -0300

    Remove commented out code

--- src/middlewares/loadGamedata.js
+++ src/middlewares/loadGamedata.js
@@ -7,13 +7,10 @@ export default store => next => action => {
     try {
 
       // Load levels and add a bit of post processing...
-      // console.log(`NETWORK NAME:`, constants.ACTIVE_NETWORK.name)
-      // console.log(`LEVELS URL:`, `../../levels/levels-${constants.ACTIVE_NETWORK.name}.json`)
       const network = constants.ACTIVE_NETWORK.name;
       const data = require(`../../gamedata/gamedata.json`)
       const deployData = require(`../../gamedata/deploy.${network}.json`)
       const levelsIn = data.levels;
-      // console.log(`LEVELS DATA:`, levelsIn, deployData)
       const levelsOut = [];
       for(let i = 0; i < levelsIn.length; i++) {
         const level = levelsIn[i];
@@ -23,7 +20,6 @@ export default store => next => action => {
       }
       action.ethernautAddress = deployData.ethernaut
       action.levels = levelsOut;
-      // console.log(`LEVELS DATA (out):`, levelsOut, action.ethernautAddress)
     } catch(e) {
       window.alert('cannot find levels data')
     }
