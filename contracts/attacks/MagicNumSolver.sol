78c216ddf6bbd3465754099797a9ae6668d178b8
Alejandro Santander <Palebluedot@gmail.com>
2018-07-09 17:48:43 -0300

    Simpler code that achieves the same thing as before

--- contracts/attacks/MagicNumSolver.sol
+++ contracts/attacks/MagicNumSolver.sol
@@ -1,40 +1,23 @@
 pragma solidity ^0.4.24;
 
 contract MagicNumSolver {
-  function createSolver() public returns (address) {
-      
-    address addr;
-    uint256 size;
-      
+  constructor() public {
     assembly {
-          
-      // This is the code we want to return.
+
+      // This is the bytecode we want the program to have:
       // 00 PUSH1 42 /* push 42 onto the stack */
       // 03 PUSH1  0 /* store 42 at memory position 0 */
       // 05 MSTORE
-      // 06 PUSH1  1 /* return 1 byte at memory offset 31 */
-      // 08 PUSH1 1f
+      // 06 PUSH1 20 /* return 32 bytes in memory */
+      // 08 PUSH1 0
       // 10 RETURN
-      // Bytecode: 0x60426000526001601ff3 (length 0x0a or 10)
+      // Bytecode: 0x604260005260206000f3 (length 0x0a or 10)
+      // Bytecode within a 32 byte word:
+      // 0x00000000000000000000000000000000000000000000604260005260206000f3 (length 0x20 or 32)
+      //                                               ^ (offset 0x16 or 22)
       
-      // With deploy code (created using evm-deploy):
-      // 0x600a80600b6000396000f360426000526001601ff3 (length 0x15 or 21)
-      
-      // Within a word:
-      // 0x0000000000000000000000600a80600b6000396000f360426000526001601ff3
-      //                         ^ (offset 0x0b or 11)     
-      
-      // Store the bytecode in memory so that it can be used to create a new "contract".
-      mstore(0, 0x600a80600b6000396000f360426000526001601ff3)
-      
-      // Create a contract whose code is what's stored at mem pos 0.
-      addr := create(0, 0x0b, 0x15)
-      
-      // Verify created contract code size.
-      size := extcodesize(addr)
+      mstore(0, 0x604260005260206000f3)
+      return(0x16, 0x0a)
     }
-    require(size <= 10);
-      
-    return addr;
   }
 }
--- contracts/levels/MagicNumFactory.sol
+++ contracts/levels/MagicNumFactory.sol
@@ -10,27 +10,20 @@ contract MagicNumFactory is Level {
 
   function validateInstance(address _instance, address) public returns (bool) {
 
-    // Retrieve program from instance.
-    address program = MagicNum(_instance).solver;
-
-    uint256 val;
+    // Retrieve the instance casted as a solver.
+    Solver solver = Solver(_instance);
+    
+    // Query the solver for the magic number.
+    bytes32 magic = solver.whatIsTheMeaningOfLife();
+    if(magic != 0x0000000000000000000000000000000000000000000000000000000000000042) return false;
+    
+    // Require the solver to have at most 10 opcodes.
     uint256 size;
-    assembly { 
-      let success := call(
-        100000, // gas limit
-        program, // target address
-        0, // cero value
-        0, // input mem offset (no input data)
-        0, // input mem length (no input length)
-        0x1f, // output mem offset
-        0x20 // output mem length
-      )
-      val := mload(0) // fetch the return val in memory
-      size := extcodesize(_instance) // read code size of the target address
+    assembly {
+      size := extcodesize(solver)
     }
     if(size > 10) return false;
-    if(val != 0x0000000000000000000000000000000000000000000000000000000000000042) return false;
-     
+    
     return true;
   }
 }
