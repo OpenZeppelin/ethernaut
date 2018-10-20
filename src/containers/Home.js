bb0a0b14e0afcae9a36ad823fc8a6c9d6d83a903
Alejandro Santander <Palebluedot@gmail.com>
2018-10-20 15:32:40 -0300

    Updated home page text.

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -42,7 +42,8 @@ class Home extends React.Component {
             </a>
           </h2>
           {/* INFO */}
-          <p>The ethernaut is a Web3/Solidity based wargame inspired on <a href="https://overthewire.org" target="_blank" rel="noopener noreferred">overthewire.org</a>, played in the Ethereum Virtual Machine. Each level is a smart contract that needs to be 'hacked' in order to advance.</p>
+          <p>The Ethernaut is a Web3/Solidity based wargame inspired on <a href="https://overthewire.org" target="_blank" rel="noopener noreferred">overthewire.org</a>, played in the Ethereum Virtual Machine. Each level is a smart contract that needs to be 'hacked'.</p>
+          <p>The game is 100% open source and all levels are contributions made by other players. Do you have an interesting idea? PRs are welcome at <a href="https://github.com/OpenZeppelin/ethernaut">github.com/OpenZeppelin/ethernaut</a>.</p>
           <p>Are you interested in smart contract development or security? Does securing the world’s blockchain infrastructure sound exciting to you? <a href="https://zeppelin.solutions/jobs" target="_blank" rel="noopener noreferred"><strong style={{ color: '#eb5424', fontWeight: 600 }}>We are hiring!</strong></a></p>
           <button
             style={{marginTop: '10px'}}
