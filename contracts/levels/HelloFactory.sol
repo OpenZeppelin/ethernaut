1d243966d256cb4612ffcbb304986705d8362f0a
Raymond Yeh <raymond_yeh@tech.gov.sg>
2019-01-02 15:55:25 +0800

    Added hello

---
+++ contracts/levels/HelloFactory.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.5.0;
+
+import './base/Level.sol';
+import './Hello.sol';
+
+contract HelloFactory is Level {
+
+  function createInstance(address _player) public payable returns (address) {
+    _player;
+    return address(new Hello('govtech_ethernaut'));
+  }
+
+  function validateInstance(address _instance, address _player) public returns (bool) {
+    _player;
+    Hello instance = Hello(_instance);
+    return instance.getCleared();
+  }
+}
