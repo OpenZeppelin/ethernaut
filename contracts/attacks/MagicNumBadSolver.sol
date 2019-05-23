7fd6e355b770e59def50a7ec6aecae97b039acd0
Paulina Blaszkiewicz <paulina.blaszk@gmail.com>
2019-05-23 10:41:40 +0200

    Update attack contracts #117

--- contracts/attacks/MagicNumBadSolver.sol
+++ contracts/attacks/MagicNumBadSolver.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.5.0;
 contract MagicNumBadSolver {
     
   function whatIsTheMeaningOfLife() public pure returns (bytes32) {
-    return 42;
+    return bytes4(int32(42));
   }
 }
