3597ab35425ec7f362c4c2d4dd6ffab951cc47aa
Paulina Blaszkiewicz <paulina.blaszk@gmail.com>
2019-05-30 12:09:12 +0200

    EthernautTestes update

--- test/EthernautTests.js
+++ test/EthernautTests.js
@@ -4,7 +4,7 @@ const DummyLevel = artifacts.require('./levels/DummyLevel.sol');
 const Dummy = artifacts.require('./levels/Dummy.sol');
 const FallbackFactory = artifacts.require('./levels/FallbackFactory.sol');
 const Manufactured = artifacts.require('./levels/Manufactured.sol');
-import expectThrow from 'zeppelin-solidity/test/helpers/expectThrow'
+const { BN, constants, expectEvent, expectRevert } = require('openzeppelin-test-helpers')
 import * as utils from './utils/TestUtils'
 
 contract('Ethernaut', function(accounts) {
@@ -25,13 +25,14 @@ contract('Ethernaut', function(accounts) {
 
     const level = await FallbackFactory.new()
     await ethernaut.registerLevel(level.address, {from: owner});
+    console.log(level.address);
 
     // Instead of solving the instance, the player manufactures an instance
     // with the desired state:
     // const instance = await utils.createLevelInstance(ethernaut, level.address, player, Fallback)
     const instance = await Manufactured.new()
 
-    await expectThrow(ethernaut.submitLevelInstance(instance.address, {from: player}))
+    await expectRevert(ethernaut.submitLevelInstance(instance.address, {from: player}), 'VM Exception while processing transaction: revert')
   });
 
   it(`should not allow player A to use player's B instance to complete a level`, async function() {
@@ -45,7 +46,7 @@ contract('Ethernaut', function(accounts) {
     console.log(`completed:`, completed)
     assert.equal(completed, true)
 
-    await expectThrow(ethernaut.submitLevelInstance(instance.address, {from: accounts[2]}))
+    await expectRevert(ethernaut.submitLevelInstance(instance.address, {from: accounts[2]}), 'VM Exception while processing transaction: revert')
   });
 
   it(`should not allow a player to generate 2 completion logs with the same instance`, async function() {
@@ -68,7 +69,7 @@ contract('Ethernaut', function(accounts) {
     assert.equal(ethCompleted, true)
 
     // Resubmit instance
-    await expectThrow(ethernaut.submitLevelInstance(instance.address))
+    await expectRevert(ethernaut.submitLevelInstance(instance.address), 'VM Exception while processing transaction: revert')
   });
 
   it(`should provide instances and verify completion`, async function() {
@@ -109,11 +110,11 @@ contract('Ethernaut', function(accounts) {
 
   it(`should not provide instances to non-registered level factories`, async function() {
     const level = await DummyLevel.new()
-    await expectThrow(ethernaut.createLevelInstance(level.address, {from: player}))
+    await expectRevert(ethernaut.createLevelInstance(level.address, {from: player}), 'VM Exception while processing transaction: revert')
   });
 
   it(`should not allow anyone but the owner to upload a level`, async function() {
     const level = await DummyLevel.new()
-    await expectThrow(ethernaut.registerLevel(level.address, {from: player}))
+    await expectRevert(ethernaut.registerLevel(level.address, {from: player}), 'VM Exception while processing transaction: revert')
   });
 });
