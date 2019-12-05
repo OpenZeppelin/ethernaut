a4890ed3d62338c51effefaac3c61a24665ca024
Dan Forbes <dforbes@smartcontract.com>
2019-12-05 13:52:54 -0800

    Fix MagicNumSolver comments

--- contracts/attacks/MagicNumSolver.sol
+++ contracts/attacks/MagicNumSolver.sol
@@ -11,9 +11,9 @@ contract MagicNumSolver {
       // 06 PUSH1 20 /* return 32 bytes in memory */
       // 08 PUSH1 0
       // 10 RETURN
-      // Bytecode: 0x604260005260206000f3 (length 0x0a or 10)
+      // Bytecode: 0x602a60005260206000f3 (length 0x0a or 10)
       // Bytecode within a 32 byte word:
-      // 0x00000000000000000000000000000000000000000000604260005260206000f3 (length 0x20 or 32)
+      // 0x00000000000000000000000000000000000000000000602a60005260206000f3 (length 0x20 or 32)
       //                                               ^ (offset 0x16 or 22)
       
       mstore(0, 0x602a60005260206000f3)
