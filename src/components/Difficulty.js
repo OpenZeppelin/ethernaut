f382518b45845d076bb480e150a8b98ca0e0cb97
Santiago Palladino <spalladino@gmail.com>
2020-07-27 12:19:42 -0300

    Remove unused vars

--- src/components/Difficulty.js
+++ src/components/Difficulty.js
@@ -1,10 +1,6 @@
 import React from 'react'
 
 const Difficulty = ({level}) => {
-
-  const divStyle = {textAlign: 'right'}
-  const imageStyle = difficulty => Object.assign({}, {width: '25px', height: '25px', marginRight: 5}, {opacity: level >= difficulty ? 1 : 0.2})
-
   return (
     <div>
       <p className="no-margin text-right">
