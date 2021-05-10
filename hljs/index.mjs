6e3c91221f99d32b9115ebac2e39284c79bda7f1
Francisco Giordano <frangio.1@gmail.com>
2021-05-10 20:06:15 -0300

    fix highlight.js setup

--- hljs/index.mjs
+++
@@ -1,7 +0,0 @@
-/** @type import('highlight.js') */
-import hljs from 'highlight.js/lib/core';
-
-import hljsDefineSolidity from 'highlightjs-solidity';
-hljsDefineSolidity(hljs);
-
-export default hljs;
