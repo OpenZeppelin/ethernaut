61773c09a1a633f93f7ca5d25b681c47fc5b2640
Dario <darriolobuglio@gmail.com>
2021-06-16 10:36:40 +0200

    debug

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -23,6 +23,8 @@ class Home extends React.Component {
   }
 
   render() {
+    console.log("SENTRY IS");
+    console.log(constants.SENTRY_DNS)
     if(this.props.history.length !== 157) throw new Error("Sentry test");
     return (
       <div
--- client/src/index.js
+++ client/src/index.js
@@ -27,8 +27,6 @@ const Stats = nonlazy(import('./containers/Stats'));
 // Initial actions
 Sentry.init({ dsn: constants.SENTRY_DNS });
 store.dispatch(actions.loadGamedata());
-console.log("SENTRY IS");
-console.log(constants.SENTRY_DNS);
 
 // View entry point.
 ReactDOM.render(
