fc08441557f4f05fe2759c50bfe2ca173ae531f5
Dario <darriolobuglio@gmail.com>
2021-07-07 12:39:14 +0200

    delete contracts from client folder

--- client/src/contracts/levels/Dummy.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.6.0;
-
-contract Dummy {
-  bool public completed;
-  function setCompleted(bool _completed) public {
-    completed = _completed;
-  }
-}
--- client/src/contracts/levels/base/Level.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.6.0;
-
-import '@openzeppelin/contracts/access/Ownable.sol';
-
-abstract contract Level is Ownable {
-  function createInstance(address _player) virtual public payable returns (address);
-  function validateInstance(address payable _instance, address _player) virtual public returns (bool);
-}
