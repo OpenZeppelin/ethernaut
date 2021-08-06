fe74123769b42a9ff632b50ab516e725d257b023
Dario <dariolobuglio@gmail.com>
2021-08-06 14:41:58 +0200

    Trick gas (#233)
    * trick gas and gasPrice
    
    * update changelog

--- client/src/utils/ethutil.js
+++ client/src/utils/ethutil.js
@@ -15,8 +15,8 @@ export const getTruffleContract = (jsonABI, defaults = {}) => {
   // const TruffleContract = require('@truffle/contract');
   
   const truffleContract = TruffleContract(jsonABI);
-  if(!defaults.gasPrice) defaults.gasPrice = 20000000000;
-  // if(!defaults.gas) defaults.gas = 200000;
+  if(!defaults.gasPrice) defaults.gasPrice = 2000000000;
+  if(!defaults.gas) defaults.gas = 2000000;
   truffleContract.defaults(defaults);
   truffleContract.setProvider(web3.currentProvider);
   return truffleContract;
