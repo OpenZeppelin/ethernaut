9d1170c79ec5ba5ddf3ad3ebc63729903d2375a6
Vicente Dragicevic <vdrg@protonmail.ch>
2021-10-05 17:39:45 -0300

    Initial draft

---
+++ contracts/contracts/levels/PuzzleWallet.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
+
+contract PuzzleWallet {
+
+    mapping(address => uint256) public balances;
+    
+    // Adapted from https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Multicall.sol#L16
+    function multicall(bytes[] calldata data) external returns (bytes[] memory results) {
+        results = new bytes[](data.length);
+
+        // Protect against reusing msg.value
+        bool depositCalled = false;
+
+        for (uint256 i = 0; i < data.length; i++) {
+            bytes memory _data = data[i];
+            bytes4 selector;
+            assembly {
+                selector := mload(add(_data, 32))
+            }
+
+            if (selector == this.deposit.selector) {
+                require(!depositCalled, "Deposit can only be called once");
+                depositCalled = true;
+            }
+
+            (bool success, bytes memory returndata) = address(this).delegatecall(data[i]);
+            if (!success) {
+                // Look for revert reason and bubble it up if present
+                if (returndata.length > 0) {
+                    // The easiest way to bubble the revert reason is using memory via assembly
+                    assembly {
+                        let returndata_size := mload(returndata)
+                        revert(add(32, returndata), returndata_size)
+                    }
+                } else {
+                    revert();
+                }
+            }
+            results[i] = returndata;
+        }
+        return results;
+    }
+
+    function deposit(uint256 amount) external payable {
+        require(amount == msg.value);
+        // Add to sender's balance
+        balances[msg.sender] = balances[msg.sender] + amount;
+    }
+    
+    function execute(address to, uint256 value, bytes calldata data) external payable returns(bytes memory) {
+        uint256 currentBalance = balances[msg.sender];
+        require(currentBalance >= value, "Insufficient balance");
+        (bool success, bytes memory result) = to.call{value: value}(data);
+        require(success, "Execution failed");
+        return result;
+    }
+}
