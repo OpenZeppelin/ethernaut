33cede8ab3cad9c56bc87cb975b5b658ba89b71d
Andres Bachfischer <andresbach@gmail.com>
2021-10-06 18:53:28 -0300

    Adapted createInstance to deploy and initialize proxy and logic

--- contracts/contracts/levels/PuzzleWalletFactory.sol
+++ contracts/contracts/levels/PuzzleWalletFactory.sol
@@ -8,13 +8,23 @@ contract PuzzleWalletFactory is Level {
   function createInstance(address /*_player*/) override public payable returns (address) {
     require(msg.value == 1 ether, "Must send 1 ETH to create instance");
 
-    PuzzleWallet puzzle = new PuzzleWallet();
+    // Deploy the Wallet logic
+    PuzzleWallet walletLogic = new PuzzleWallet();
+    walletLogic.init();
 
-    puzzle.addToWhitelist(address(this));
+    // Proxy instance
+    bytes data= abi.encodeWithSelector(PuzzleWallet.init.selector);
+    PuzzleProxy proxy = new PuzzleProxy(address(walletLogic), data, address(this));
 
-    puzzle.deposit{value: 1 ether}(1 ether);
+    data = abi.encodeWithSelector(PuzzleWallet.addToWhitelist.selector, (address(this)));
+    (bool success, bytes memory result) = address(proxy).call(data);
+    require(success, "Execution failed");
 
-    return address(puzzle);
+    data = abi.encodeWithSelector(PuzzleWallet.deposit.selector, 1 ether);
+    (success, result) = address(proxy).call{ value: msg.value }(data);
+    require(success, "Execution failed");
+
+    return address(proxy);
   }
 
   function validateInstance(address payable _instance, address /*_player*/) override public returns (bool) {
