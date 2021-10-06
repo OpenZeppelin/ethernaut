1b62bbe76233e20266f65aeddbc9d95199b834a9
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2021-10-06 15:21:45 -0400

    Add proxy contract

--- contracts/contracts/levels/PuzzleWallet.sol
+++ contracts/contracts/levels/PuzzleWallet.sol
@@ -4,6 +4,64 @@ pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
+contract Proxy {
+    address public pendingAdmin;
+    bytes32 public constant ADMIN_SLOT = 0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103;
+    bytes32 public constant IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
+
+    constructor(address _implementation, address _admin) public {
+        bytes32 adminSlot = ADMIN_SLOT;
+        bytes32 implSlot = IMPLEMENTATION_SLOT;
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            sstore(implSlot, _implementation)
+            sstore(adminSlot, _admin)
+        }
+    }
+
+    modifier onlyAdmin {
+      bytes32 adminSlot = ADMIN_SLOT;
+      address admin;
+
+      assembly {
+          admin := sload(adminSlot)
+      }
+
+      require(msg.sender == admin, "Caller is not the admin");
+      _;
+    }
+
+    // Anyone can propose a new admin, but the current admin can just reject it or never accept it
+    function proposeNewAdmin(address _newAdmin) external {
+      pendingAdmin = _newAdmin;
+    }
+
+    function updateAdmin() external onlyAdmin {
+      bytes32 slot = ADMIN_SLOT;
+      address newAdmin = pendingAdmin;
+      // solhint-disable-next-line no-inline-assembly
+      assembly {
+          sstore(slot, newAdmin)
+      }
+    }
+
+    fallback() external payable {
+        // solhint-disable-next-line no-inline-assembly
+        bytes32 slot = IMPLEMENTATION_SLOT;
+        address impl;
+        assembly {
+            impl := sload(slot)
+            calldatacopy(0, 0, calldatasize())
+            let result := delegatecall(gas(), impl, 0, calldatasize(), 0, 0)
+            returndatacopy(0, 0, returndatasize())
+
+            switch result
+            case 0 { revert(0, returndatasize()) }
+            default { return(0, returndatasize()) }
+        }
+    }
+}
+
 contract PuzzleWallet {
     using SafeMath for uint256;
     address public owner;
