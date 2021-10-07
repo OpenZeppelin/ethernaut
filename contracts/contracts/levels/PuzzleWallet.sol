a682cbf1c65c3dd7aac8e6e90a8584e9f3c33424
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2021-10-07 09:38:39 -0400

    Change params order in constructor

--- contracts/contracts/levels/PuzzleWallet.sol
+++ contracts/contracts/levels/PuzzleWallet.sol
@@ -9,7 +9,7 @@ contract PuzzleProxy is UpgradeableProxy {
     address public pendingAdmin;
     address public admin;
 
-    constructor(address _implementation, bytes memory _initData, address _admin) UpgradeableProxy(_implementation, _initData) public {
+    constructor(address _admin, address _implementation, bytes memory _initData) UpgradeableProxy(_implementation, _initData) public {
         admin = _admin;
     }
 
