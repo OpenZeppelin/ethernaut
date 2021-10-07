0074a861aae1a46b29d5ac21187d9df7e228857f
Dario <dariolobuglio@gmail.com>
2021-10-07 17:49:42 +0200

    add more resources

--- contracts/contracts/levels/Motorbike.sol
+++ contracts/contracts/levels/Motorbike.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/proxy/Initializable.sol";
 
 contract Motorbike {
+    // keccak-256 hash of "eip1967.proxy.implementation" subtracted by 1
     bytes32 internal constant _IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
     
     struct AddressSlot {
@@ -50,6 +51,7 @@ contract Motorbike {
 }
 
 contract Engine is Initializable {
+    // keccak-256 hash of "eip1967.proxy.implementation" subtracted by 1
     bytes32 internal constant _IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
 
     address public upgrader;
