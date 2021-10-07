20d7ff9c9490d52a1c8ad45f4ae86db1dcff0357
Andres Bachfischer <andresbach@gmail.com>
2021-10-07 01:05:36 -0300

    Draft version of the win procedure

--- contracts/test/levels/PuzzleWallet.test.js
+++ contracts/test/levels/PuzzleWallet.test.js
@@ -28,18 +28,36 @@ contract('PuzzleWallet', function(accounts) {
     assert.equal(await instance.owner(), level.address)
     assert.equal(web3.utils.toWei('1', 'ether'), (await instance.balances(level.address)).toString())
     
-    // TODO: take ownership in a real way
-    await instance.takeOwnership({from: player})
+    // check that PuzzleProxy.pendingAdmin is factory
+    assert.equal(owner, await instance.owner())
+
+    // call the PuzzleProxy.proposeNewAdmin(player)
+    // need to use artifact from the proxy (not the wallet)
+
+    // check that the player has placed their address in the owner slot
     assert.equal(player, await instance.owner())
-    
-    // Player whitelists herself
+
+    // check that player is not whitelisted yet
+    assert.notequal(true, await instance.whitelisted(player))
+
+    // whitelist itself by PuzzleWallet.addToWhitelist(player)
     await instance.addToWhitelist(player, { from: player })
 
-    const calls = [
-      // TODO: calls
-    ]
+    // deposit 1 eth with PuzzleWallet.deposit{ 1 ether }(1 ether) from player
+    await instance.deposit(web3.utils.toWei('1', 'ether'), {from: player, value: web3.utils.toWei('1', 'ether')})
 
+    // build the calldata
+    // call PuzzleWallet.multicall(bytes(2x PuzzleWallet.execute.selector(Proxy.address, 1 ether, "")))
+    const executeSelector = web3.eth.abi.encodeFunctionSignature("execute(address,uint256,bytes)");
+    // call = []
+
+    // call the multicall with the data
     await instance.multicall(calls)
+
+    // check that PuzzleWallet.balance == zero
+    assert.isZero(await web3.eth.getBalance(instance))
+
+    // another step with dust?
 
     // Factory check
     const ethCompleted = await utils.submitLevelInstance(
