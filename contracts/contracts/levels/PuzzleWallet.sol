dbad884640b89c73ade10db75f99d1a71ed104ab
Dario <dariolobuglio@gmail.com>
2021-10-15 10:49:44 +0200

    OpenZeppelin Hackathon - Puzzle wallet (#277)
    * Initial draft
    
    * Add factory
    
    * Level setup/description/etc
    
    * Add SafeMath
    
    * Add test scaffold
    
    * Add proxy contract
    
    * Add real multicall to test
    
    * Use OZ Upgradeable contract
    
    * Redefine admin variable
    - Redefine admin address to follow the storage layout order and not a random slot
    - Add a totalBalance variable to the PuzzleWallet contract
    
    * Clean up proxy
    
    * Style fixes
    
    * Add bachi audit notes :P
    
    * Add comment in proposeNewAdmin function
    
    * Changing constructor to init to be used with proxy
    
    * Initialize the proxy with the logic
    
    * Adapted createInstance to deploy and initialize proxy and logic
    
    * Modify function name
    
    * Consistency in variable names
    
    * Set a initial check if the player passed the game
    
    * Draft version of the win procedure
    
    * Change params order in constructor
    
    * Add upgradeTo function
    
    * Add _maxBalance parameter to init function
    
    * Changed order of params and new init
    
    * Fix compile errors and tests
    
    * Added initial version of pre and post history
    
    * Added disclaimer for delegatecall
    
    * Shrink functions
    
    * Remove blankspace
    
    * Minor change
    
    * Update puzzle_wallet.md
    
    * Update puzzle_wallet_complete.md
    
    * Update puzzle_wallet.md
    
    * Update client/src/gamedata/en/descriptions/levels/puzzle_wallet.md
    
    Co-authored-by: Andres Bachfischer <38696580+andresbach@users.noreply.github.com>
    
    * Add adminship takeover
    
    * Clean up of tests and factory
    
    * Add error messages in tests
    
    * Remove unused variable
    
    * Remove unnecessary comment
    
    * Change upgrade address
    
    * Change line in puzzle_wallet.md
    
    * Change validateInstance return var
    
    * Add  semicolons in tests
    
    * Remove eth restriction
    
    * Pump the difficulty
    
    * Change authors names
    
    * Fix maxBalance check
    
    * fix gamedata and level, adding jap files
    
    * restore rinkeby
    
    * deploy level on rinkeby
    
    Co-authored-by: Vicente Dragicevic <vdrg@protonmail.ch>
    Co-authored-by: Juan Bautista Carpanelli <juanbanelli@gmail.com>
    Co-authored-by: Andres Bachfischer <38696580+andresbach@users.noreply.github.com>
    Co-authored-by: Andres Bachfischer <andresbach@gmail.com>

---
+++ contracts/contracts/levels/PuzzleWallet.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/proxy/UpgradeableProxy.sol";
+
+contract PuzzleProxy is UpgradeableProxy {
+    address public pendingAdmin;
+    address public admin;
+
+    constructor(address _admin, address _implementation, bytes memory _initData) UpgradeableProxy(_implementation, _initData) public {
+        admin = _admin;
+    }
+
+    modifier onlyAdmin {
+      require(msg.sender == admin, "Caller is not the admin");
+      _;
+    }
+
+    function proposeNewAdmin(address _newAdmin) external {
+        pendingAdmin = _newAdmin;
+    }
+
+    function approveNewAdmin(address _expectedAdmin) external onlyAdmin {
+        require(pendingAdmin == _expectedAdmin, "Expected new admin by the current admin is not the pending admin");
+        admin = pendingAdmin;
+    }
+
+    function upgradeTo(address _newImplementation) external onlyAdmin {
+        _upgradeTo(_newImplementation);
+    }
+}
+
+contract PuzzleWallet {
+    using SafeMath for uint256;
+    address public owner;
+    uint256 public maxBalance;
+    mapping(address => bool) public whitelisted;
+    mapping(address => uint256) public balances;
+
+    function init(uint256 _maxBalance) public {
+        require(maxBalance == 0, "Already initialized");
+        maxBalance = _maxBalance;
+        owner = msg.sender;
+    }
+
+    modifier onlyWhitelisted {
+        require(whitelisted[msg.sender], "Not whitelisted");
+        _;
+    }
+
+    function setMaxBalance(uint256 _maxBalance) external onlyWhitelisted {
+      require(address(this).balance == 0, "Contract balance is not 0");
+      maxBalance = _maxBalance;
+    }
+
+    function addToWhitelist(address addr) external {
+        require(msg.sender == owner, "Not the owner");
+        whitelisted[addr] = true;
+    }
+
+    function deposit() external payable onlyWhitelisted {
+      require(address(this).balance <= maxBalance, "Max balance reached");
+      balances[msg.sender] = balances[msg.sender].add(msg.value);
+    }
+
+    function execute(address to, uint256 value, bytes calldata data) external payable onlyWhitelisted {
+        require(balances[msg.sender] >= value, "Insufficient balance");
+        balances[msg.sender] = balances[msg.sender].sub(value);
+        (bool success, ) = to.call{ value: value }(data);
+        require(success, "Execution failed");
+    }
+
+    function multicall(bytes[] calldata data) external payable onlyWhitelisted {
+        bool depositCalled = false;
+        for (uint256 i = 0; i < data.length; i++) {
+            bytes memory _data = data[i];
+            bytes4 selector;
+            assembly {
+                selector := mload(add(_data, 32))
+            }
+            if (selector == this.deposit.selector) {
+                require(!depositCalled, "Deposit can only be called once");
+                // Protect against reusing msg.value
+                depositCalled = true;
+            }
+            (bool success, ) = address(this).delegatecall(data[i]);
+            require(success, "Error while delegating call");
+        }
+    }
+}
