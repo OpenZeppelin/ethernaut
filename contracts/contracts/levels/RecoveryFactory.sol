2c8f408e85e9126fba89c0a7507b88c358dad919
xaler <dariolobuglio@gmail.com>
2021-10-15 17:34:30 +0200

    deploy rinkeby

--- contracts/contracts/levels/RecoveryFactory.sol
+++ contracts/contracts/levels/RecoveryFactory.sol
@@ -15,15 +15,15 @@ contract RecoveryFactory is Level {
     // create a simple token 
     recoveryInstance.generateToken("InitialToken", uint(100000));
     // the lost address
-    lostAddress[_player] = address(uint160(uint256(keccak256(abi.encodePacked(uint8(0xd6), uint8(0x94), recoveryInstance, uint8(0x01))))));
+    lostAddress[address(recoveryInstance)] = address(uint160(uint256(keccak256(abi.encodePacked(uint8(0xd6), uint8(0x94), recoveryInstance, uint8(0x01))))));
     // Send it some ether
-    (bool result,) = lostAddress[_player].call{value:0.5 ether}("");
+    (bool result,) = lostAddress[address(recoveryInstance)].call{value:0.5 ether}("");
     require(result);
 
     return address(recoveryInstance);
   }
 
-  function validateInstance(address payable, address _player) override public returns (bool) {
-    return address(lostAddress[_player]).balance == 0;
+  function validateInstance(address payable _instance, address _player) override public returns (bool) {
+    return address(lostAddress[_instance]).balance == 0;
   }
 }
