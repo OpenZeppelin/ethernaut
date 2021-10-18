cb6cc64d7d789e2b94151979cdb7f6ee036ca5d8
Dario <dariolobuglio@gmail.com>
2021-10-18 08:55:12 +0200

    Fix recovery factory (#279)
    * fix and deploy
    
    * Update contracts/contracts/levels/RecoveryFactory.sol
    
    Co-authored-by: Francisco Giordano <frangio.1@gmail.com>
    
    * Update contracts/contracts/levels/RecoveryFactory.sol
    
    Co-authored-by: Francisco Giordano <frangio.1@gmail.com>
    
    * deploy rinkeby
    
    * Update contracts/contracts/levels/RecoveryFactory.sol
    
    Co-authored-by: Francisco Giordano <frangio.1@gmail.com>

--- contracts/contracts/levels/RecoveryFactory.sol
+++ contracts/contracts/levels/RecoveryFactory.sol
@@ -7,27 +7,23 @@ import './Recovery.sol';
 
 contract RecoveryFactory is Level {
 
-  address lostAddress;
+  mapping (address => address) lostAddress;
 
-  function createInstance(address) override public payable returns (address) {
+  function createInstance(address _player) override public payable returns (address) {
     Recovery recoveryInstance;
     recoveryInstance = new Recovery();
     // create a simple token 
     recoveryInstance.generateToken("InitialToken", uint(100000));
     // the lost address
-    lostAddress = address(uint160(uint256(keccak256(abi.encodePacked(uint8(0xd6), uint8(0x94), recoveryInstance, uint8(0x01))))));
+    lostAddress[address(recoveryInstance)] = address(uint160(uint256(keccak256(abi.encodePacked(uint8(0xd6), uint8(0x94), recoveryInstance, uint8(0x01))))));
     // Send it some ether
-    (bool result,) = lostAddress.call{value:0.5 ether}("");
+    (bool result,) = lostAddress[address(recoveryInstance)].call{value:0.5 ether}("");
     require(result);
 
     return address(recoveryInstance);
   }
 
   function validateInstance(address payable _instance, address) override public returns (bool) {
-    require(_instance != address(0)); // Suppress solidity warning. 
-    if (address(lostAddress).balance == 0) {
-      return true;
-    }
-    return false;
+    return address(lostAddress[_instance]).balance == 0;
   }
 }
