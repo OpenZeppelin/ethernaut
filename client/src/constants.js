9a4d855143c2c7e53c422c0253341acdf26be70e
Dario <dariolobuglio@gmail.com>
2021-11-25 09:33:34 +0100

    Update constants.js

--- client/src/constants.js
+++ client/src/constants.js
@@ -65,6 +65,6 @@ export const SHOW_ALL_COMPLETE_DESCRIPTIONS = false             /* FALSE on prod
 export const SHOW_VERSION = true                                /* TRUE on production */
 // export const ACTIVE_NETWORK = NETWORKS.ROPSTEN
 export const ACTIVE_NETWORK = NETWORKS.RINKEBY
-export const ACTIVE_NETWORK = NETWORKS.LOCAL
+// export const ACTIVE_NETWORK = NETWORKS.LOCAL
 // -----------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------
