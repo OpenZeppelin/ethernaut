d0dd78d694c7db072df7b2ae4da9f843ec802cc6
Dario <dariolobuglio@gmail.com>
2022-05-13 20:00:45 +0200

    Fix Dex / Dex 2 (#345)
    * Reverting to the original design but prevents anyone from calling the public overloaded approve function with an owner address the same as the level instance.
    
    * deploy factories on rinkeby
    
    Co-authored-by: Jeremy Felder <jeremy.felder1@gmail.com>

--- contracts/contracts/levels/DexTwo.sol
+++ contracts/contracts/levels/DexTwo.sol
@@ -4,35 +4,38 @@ pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import '@openzeppelin/contracts/math/SafeMath.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
 
-contract DexTwo  {
+contract DexTwo is Ownable {
   using SafeMath for uint;
   address public token1;
   address public token2;
-  constructor(address _token1, address _token2) public {
+  constructor() public {}
+
+  function setTokens(address _token1, address _token2) public onlyOwner {
     token1 = _token1;
     token2 = _token2;
   }
 
-  function swap(address from, address to, uint amount) public {
-    require(IERC20(from).balanceOf(msg.sender) >= amount, "Not enough to swap");
-    uint swap_amount = get_swap_amount(from, to, amount);
-    IERC20(from).transferFrom(msg.sender, address(this), amount);
-    IERC20(to).approve(address(this), swap_amount);
-    IERC20(to).transferFrom(address(this), msg.sender, swap_amount);
-  }
-
-  function add_liquidity(address token_address, uint amount) public{
+  function add_liquidity(address token_address, uint amount) public onlyOwner {
     IERC20(token_address).transferFrom(msg.sender, address(this), amount);
   }
+  
+  function swap(address from, address to, uint amount) public {
+    require(IERC20(from).balanceOf(msg.sender) >= amount, "Not enough to swap");
+    uint swapAmount = getSwapAmount(from, to, amount);
+    IERC20(from).transferFrom(msg.sender, address(this), amount);
+    IERC20(to).approve(address(this), swapAmount);
+    IERC20(to).transferFrom(address(this), msg.sender, swapAmount);
+  } 
 
-  function get_swap_amount(address from, address to, uint amount) public view returns(uint){
+  function getSwapAmount(address from, address to, uint amount) public view returns(uint){
     return((amount * IERC20(to).balanceOf(address(this)))/IERC20(from).balanceOf(address(this)));
   }
 
   function approve(address spender, uint amount) public {
-    SwappableTokenTwo(token1).approve(spender, amount);
-    SwappableTokenTwo(token2).approve(spender, amount);
+    SwappableTokenTwo(token1).approve(msg.sender, spender, amount);
+    SwappableTokenTwo(token2).approve(msg.sender, spender, amount);
   }
 
   function balanceOf(address token, address account) public view returns (uint){
@@ -41,7 +44,14 @@ contract DexTwo  {
 }
 
 contract SwappableTokenTwo is ERC20 {
-  constructor(string memory name, string memory symbol, uint initialSupply) public ERC20(name, symbol) {
+  address private _dex;
+  constructor(address dexInstance, string memory name, string memory symbol, uint initialSupply) public ERC20(name, symbol) {
         _mint(msg.sender, initialSupply);
+        _dex = dexInstance;
+  }
+
+  function approve(address owner, address spender, uint256 amount) public returns(bool){
+    require(owner != _dex, "InvalidApprover");
+    super._approve(owner, spender, amount);
   }
 }
