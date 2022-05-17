7235464b7787ed738ce2e7ee456409e3328e80f4
The dragon <mrbeesizzle@gmail.com>
2022-05-17 21:28:33 +0300

    now the project version is 0.6.12

--- contracts/contracts/levels/PriceItFactory.sol
+++ contracts/contracts/levels/PriceItFactory.sol
@@ -3,7 +3,7 @@ pragma solidity 0.6.12;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "openzeppelin4/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./PriceIt.sol";
 import './base/Level.sol';
 
