acc197a5801e212b735246de96ddafc0b4b74cb2
The dragon <mrbeesizzle@gmail.com>
2022-05-17 21:26:51 +0300

    now it is the correct solidiy version

--- contracts/contracts/levels/PriceIt.sol
+++ contracts/contracts/levels/PriceIt.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+pragma solidity 0.6.12;
+import "openzeppelin4/token/ERC20/IERC20.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -23,7 +23,7 @@ module.exports = {
             runs: 1000,
           },
         }
-      }
+      },
     ]
   },
   paths: {
