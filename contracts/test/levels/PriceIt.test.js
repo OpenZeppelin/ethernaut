b8a8ae3ffe318c23e5511dfee87c3eb0f9df5b12
The dragon <mrbeesizzle@gmail.com>
2022-05-18 21:45:27 +0300

    Created a great test suite

--- contracts/test/levels/PriceIt.test.js
+++ contracts/test/levels/PriceIt.test.js
@@ -1,45 +1,36 @@
-const PriceIt = artifacts.require('./levels/PriceIt.sol')
-const PriceItFactory = artifacts.require('./levels/PriceItFactory.sol')
+const PriceIt = artifacts.require('./levels/PriceIt.sol');
+const PriceItFactory = artifacts.require('./levels/PriceItFactory.sol');
+const Ethernaut = artifacts.require('./Ethernaut.sol');
+const { BN, constants, expectEvent, expectRevert } = require('openzeppelin-test-helpers');
+const utils = require('../utils/TestUtils');
 
-const Ethernaut = artifacts.require('./Ethernaut.sol')
-const { BN, constants, expectEvent, expectRevert } = require('openzeppelin-test-helpers')
-const utils = require('../utils/TestUtils')
+contract('PriceIt', function (accounts) {
+  let ethernaut;
+  let level;
+  let owner = accounts[1];
+  let player = accounts[0];
 
-
-contract('PriceIt', function(accounts) {
-
-  let ethernaut
-  let level
-  let owner = accounts[1]
-  let player = accounts[0]
-
-  before(async function() {
+  before(async function () {
     ethernaut = await Ethernaut.new();
-    level = await PriceItFactory.new()
-    await ethernaut.registerLevel(level.address)
+    level = await PriceItFactory.new();
+    await ethernaut.registerLevel(level.address);
   });
 
-  it('should fail if the player did not solve the level', async function() {
-    const instance = await utils.createLevelInstance(ethernaut, level.address, player, PriceIt)
-
-    const completed = await utils.submitLevelInstance(
-      ethernaut,
-      level.address,
-      instance.address,
-      player
-    )
-
-    assert.isFalse(completed)
-  });
-
-
-  it('should allow the player to solve the level', async function() {
+  it('should fail if the player did not solve the level', async function () {
     const instance = await utils.createLevelInstance(ethernaut, level.address, player, PriceIt);
 
-    const address = await web3.eth.getStorageAt(instance.address, "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc")
+    const completed = await utils.submitLevelInstance(ethernaut, level.address, instance.address, player);
+
+    assert.isFalse(completed);
+  });
+
+  it('should allow the player to solve the level', async function () {
+    const instance = await utils.createLevelInstance(ethernaut, level.address, player, PriceIt);
+
+    const address = await web3.eth.getStorageAt(instance.address, '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc');
     let string = '0x' + JSON.stringify(address).substr(27, 40);
     string = web3.utils.toChecksumAddress(string);
-    
+
     const attacker = await MotorbikeAttack.new(string);
 
     // ATTACK FIRST STEP: Take control over upgradeability functionality
@@ -48,14 +39,8 @@ contract('PriceIt', function(accounts) {
     // ATTACK SECOND STEP: Destroy the implementation
     await attacker.destroy();
 
-    const completed = await utils.submitLevelInstance(
-      ethernaut,
-      level.address,
-      instance.address,
-      player
-    )
-    
-    assert.isTrue(completed)
-  });
+    const completed = await utils.submitLevelInstance(ethernaut, level.address, instance.address, player);
 
+    assert.isTrue(completed);
+  });
 });
