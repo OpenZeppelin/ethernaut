d70d74363fb119ef35ff95dec6dba82fbf57dfbb
The dragon <mrbeesizzle@gmail.com>
2022-05-18 14:22:01 +0300

    updated the pragma to the common syntax

--- contracts/contracts/levels/PriceIt.sol
+++ contracts/contracts/levels/PriceIt.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.6.12;
+pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
