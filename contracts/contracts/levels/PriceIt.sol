b6e6a16268f2b232ae38c89ba367fc1988976917
The dragon <mrbeesizzle@gmail.com>
2022-05-20 17:00:50 +0300

    new priceit level with non sorted tokens

--- contracts/contracts/levels/PriceIt.sol
+++ contracts/contracts/levels/PriceIt.sol
@@ -17,24 +17,24 @@ contract PriceIt {
     IERC20 _token1,
     IERC20 _token2
   ) public {
-    // You can assume the tokens are sorted, i.e. _token0 < _token1 < _token2.
     (token0, token1, token2) = (_token0, _token1, _token2);
   }
 
   function buyToken(uint256 inputAmount, IERC20 inputToken) external {
     IERC20 outputToken = inputToken == token0 ? token1 : token0;
-    uint256 outputAmount = getTokenPrice(inputAmount, inputToken);
+    uint256 outputAmount = getTokenPrice(inputAmount, address(inputToken));
     inputToken.transferFrom(msg.sender, address(this), inputAmount);
     outputToken.transfer(msg.sender, outputAmount);
   }
 
-  function getTokenPrice(uint256 inputAmount, IERC20 inputToken) private view returns (uint256) {
+  function getTokenPrice(uint256 inputAmount, address inputToken) private view returns (uint256) {
     IUniswapV2Pair pair = IUniswapV2Pair(uniFactory.getPair(address(token0), address(token1)));
-    (uint256 res0, uint256 res1, ) = pair.getReserves();
-    if (inputToken == token0) {
-      return uniRouter.getAmountOut(inputAmount, res0, res1);
-    } else if (inputToken == token1) {
-      return uniRouter.getAmountOut(inputAmount, res1, res0);
+    (uint resA, uint resB, ) = pair.getReserves();
+    (address tokenA, address tokenB) = (pair.token0(), pair.token1());
+    if (inputToken == tokenA) {
+      return uniRouter.getAmountOut(inputAmount, resA, resB);
+    } else if (inputToken == tokenB) {
+      return uniRouter.getAmountOut(inputAmount, resB, resA);
     } else {
       revert('Input token is not part of the token0/token1 pair.');
     }
