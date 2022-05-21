2cb02820d5aebc43c957be440f91ccc8c25c529a
The dragon <mrbeesizzle@gmail.com>
2022-05-21 13:52:36 +0300

    using promise all correctly

--- contracts/test/levels/PriceIt.test.js
+++ contracts/test/levels/PriceIt.test.js
@@ -18,12 +18,15 @@ contract('PriceIt', function (accounts) {
 
   it('should fail if the player did not solve the level', async function () {
     const instance = await utils.createLevelInstance(ethernaut, level.address, player, PriceIt);
-
     const completed = await utils.submitLevelInstance(ethernaut, level.address, instance.address, player);
-
     assert.isFalse(completed);
   });
-  
+
+  it("should fail if the paired didn't got created", async function () {
+    const instance = await utils.createLevelInstance(ethernaut, level.address, player, PriceIt);
+    const [token0, token1, token2] = await Promise.all([instance.token0(), instance.token1(), instance.token2()]);
+  });
+
   // it('should allow the player to solve the level', async function () {
   //   const instance = await utils.createLevelInstance(ethernaut, level.address, player, PriceIt);
 
