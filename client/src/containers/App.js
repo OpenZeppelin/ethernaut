22a2588898c3668edd68a741dff394c73757e719
Dario <dariolobuglio@gmail.com>
2022-07-11 12:00:19 +0200

    remove unused header from app

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Mosaic from "./Mosaic";
-import Header from "./Header";
 import ReactGA from "react-ga";
 import * as constants from "../constants";
 import { loadTranslations } from "../utils/translations";
