9397bd5d2879f7e79e5199294e0f0b1c6132d1da
Krishnendu Sengupta <k.sengupta.29.11@gmail.com>
2022-10-25 16:02:38 +0530

    removed constructor visibility

--- contracts/contracts/helpers/UpgradeableProxy-08.sol
+++ contracts/contracts/helpers/UpgradeableProxy-08.sol
@@ -6,6 +6,7 @@ import "openzeppelin-contracts-08/proxy/Proxy.sol";
 import "openzeppelin-contracts-08/utils/Address.sol";
 
 /**
+ * 
  * @dev This contract implements an upgradeable proxy. It is upgradeable because calls are delegated to an
  * implementation address that can be changed. This address is stored in storage in the location specified by
  * https://eips.ethereum.org/EIPS/eip-1967[EIP1967], so that it doesn't conflict with the storage layout of the
@@ -21,7 +22,7 @@ contract UpgradeableProxy is Proxy {
      * If `_data` is nonempty, it's used as data in a delegate call to `_logic`. This will typically be an encoded
      * function call, and allows initializating the storage of the proxy like a Solidity constructor.
      */
-    constructor(address _logic, bytes memory _data) public payable {
+    constructor(address _logic, bytes memory _data) public {
         assert(_IMPLEMENTATION_SLOT == bytes32(uint256(keccak256("eip1967.proxy.implementation")) - 1));
         _setImplementation(_logic);
         if(_data.length > 0) {
