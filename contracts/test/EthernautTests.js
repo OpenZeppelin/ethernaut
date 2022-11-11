17133ee5cfe2e02a78f1e5974d140f1f9d9ca843
murat <murat@murats-mbp.local>
2022-11-11 13:31:50 +0300

    @fix: name convention

--- contracts/test/EthernautTests.js
+++ contracts/test/EthernautTests.js
@@ -4,11 +4,10 @@ const DummyFactory = artifacts.require('./levels/DummyFactory.sol');
 const Dummy = artifacts.require('./levels/Dummy.sol');
 const FallbackFactory = artifacts.require('./levels/FallbackFactory.sol');
 const Manufactured = artifacts.require('./levels/Manufactured.sol');
-const { expectRevert } = require('openzeppelin-test-helpers')
-const utils = require('./utils/TestUtils')
+const { expectRevert } = require('openzeppelin-test-helpers');
+const utils = require('./utils/TestUtils');
 
-contract('Ethernaut', function(accounts) {
-
+contract('Ethernaut', function (accounts) {
   // ----------------------------------
   // Before
   // ----------------------------------
@@ -17,100 +16,125 @@ contract('Ethernaut', function(accounts) {
   let player = accounts[1];
   let ethernaut;
 
-  before(async function() {
+  before(async function () {
     ethernaut = await Ethernaut.new();
   });
 
-  it(`should not allow a player to manufacture a solution instance`, async function() {
-
-    const level = await FallbackFactory.new()
-    await ethernaut.registerLevel(level.address, {from: owner});
+  it(`should not allow a player to manufacture a solution instance`, async function () {
+    const level = await FallbackFactory.new();
+    await ethernaut.registerLevel(level.address, { from: owner });
 
     // Instead of solving the instance, the player manufactures an instance
     // with the desired state:
     // const instance = await utils.createLevelInstance(ethernaut, level.address, player, Fallback)
-    const instance = await Manufactured.new()
+    const instance = await Manufactured.new();
 
-    await expectRevert.unspecified(ethernaut.submitLevelInstance(instance.address, {from: player}))
+    await expectRevert.unspecified(
+      ethernaut.submitLevelInstance(instance.address, { from: player })
+    );
   });
 
-  it(`should not allow player A to use player's B instance to complete a level`, async function() {
+  it(`should not allow player A to use player's B instance to complete a level`, async function () {
+    const level = await DummyFactory.new();
+    await ethernaut.registerLevel(level.address, { from: owner });
 
-    const level = await DummyFactory.new()
-    await ethernaut.registerLevel(level.address, {from: owner});
-
-    const instance = await utils.createLevelInstance(ethernaut, level.address, player, Dummy)
+    const instance = await utils.createLevelInstance(
+      ethernaut,
+      level.address,
+      player,
+      Dummy
+    );
     await instance.setCompleted(true);
     const completed = await instance.completed();
-    assert.equal(completed, true)
+    assert.equal(completed, true);
 
-    await expectRevert.unspecified(ethernaut.submitLevelInstance(instance.address, {from: accounts[2]}))
+    await expectRevert.unspecified(
+      ethernaut.submitLevelInstance(instance.address, { from: accounts[2] })
+    );
   });
 
-  it(`should not allow a player to generate 2 completion logs with the same instance`, async function() {
+  it(`should not allow a player to generate 2 completion logs with the same instance`, async function () {
+    const level = await DummyFactory.new();
+    await ethernaut.registerLevel(level.address, { from: owner });
 
-    const level = await DummyFactory.new()
-    await ethernaut.registerLevel(level.address, {from: owner});
-
-    const instance = await utils.createLevelInstance(ethernaut, level.address, player, Dummy)
+    const instance = await utils.createLevelInstance(
+      ethernaut,
+      level.address,
+      player,
+      Dummy
+    );
     await instance.setCompleted(true);
     const completed = await instance.completed();
-    assert.equal(completed, true)
+    assert.equal(completed, true);
 
     const ethCompleted = await utils.submitLevelInstance(
       ethernaut,
       level.address,
       instance.address,
       player
-    )
-    assert.equal(ethCompleted, true)
+    );
+    assert.equal(ethCompleted, true);
 
     // Resubmit instance
-    await expectRevert.unspecified(ethernaut.submitLevelInstance(instance.address))
+    await expectRevert.unspecified(
+      ethernaut.submitLevelInstance(instance.address)
+    );
   });
 
-  it(`should provide instances and verify completion`, async function() {
+  it(`should provide instances and verify completion`, async function () {
+    const level = await DummyFactory.new();
+    await ethernaut.registerLevel(level.address, { from: owner });
 
-    const level = await DummyFactory.new()
-    await ethernaut.registerLevel(level.address, {from: owner});
-
-    const instance = await utils.createLevelInstance(ethernaut, level.address, player, Dummy)
+    const instance = await utils.createLevelInstance(
+      ethernaut,
+      level.address,
+      player,
+      Dummy
+    );
     await instance.setCompleted(true);
     const completed = await instance.completed();
-    assert.equal(completed, true)
+    assert.equal(completed, true);
 
     const ethCompleted = await utils.submitLevelInstance(
       ethernaut,
       level.address,
       instance.address,
       player
-    )
-    assert.equal(ethCompleted, true)
+    );
+    assert.equal(ethCompleted, true);
   });
 
-  it(`should provide instances and verify non-complettion`, async function() {
+  it(`should provide instances and verify non-complettion`, async function () {
+    const level = await DummyFactory.new();
+    await ethernaut.registerLevel(level.address, { from: owner });
 
-    const level = await DummyFactory.new()
-    await ethernaut.registerLevel(level.address, {from: owner});
-
-    const instance = await utils.createLevelInstance(ethernaut, level.address, player, Dummy)
+    const instance = await utils.createLevelInstance(
+      ethernaut,
+      level.address,
+      player,
+      Dummy
+    );
 
     const completed = await utils.submitLevelInstance(
       ethernaut,
       level.address,
       instance.address,
       player
-    )
-    assert.equal(completed, false)
+    );
+    assert.equal(completed, false);
   });
 
-  it(`should not provide instances to non-registered level factories`, async function() {
-    const level = await DummyFactory.new()
-    await expectRevert.unspecified(ethernaut.createLevelInstance(level.address, {from: player}))
+  it(`should not provide instances to non-registered level factories`, async function () {
+    const level = await DummyFactory.new();
+    await expectRevert.unspecified(
+      ethernaut.createLevelInstance(level.address, { from: player })
+    );
   });
 
-  it(`should not allow anyone but the owner to upload a level`, async function() {
-    const level = await DummyFactory.new()
-    await expectRevert.unspecified(ethernaut.registerLevel(level.address, {from: player}))
+  it(`should not allow anyone but the owner to upload a level`, async function () {
+    const level = await DummyFactory.new();
+    await expectRevert.unspecified(
+      ethernaut.registerLevel(level.address, { from: player })
+    );
   });
 });
