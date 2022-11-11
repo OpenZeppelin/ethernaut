60f111604ad7209b9cf73740af7334887a5eb4e5
murat <murat@murats-MacBook-Pro.local>
2022-11-11 11:18:25 +0300

    @feat: init stats contract at Proxy

--- contracts/contracts/Proxy/Proxy.sol
+++ contracts/contracts/Proxy/Proxy.sol
@@ -3,7 +3,15 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
 contract ProxyStats is TransparentUpgradeableProxy {
-    constructor(address _impl, address _admin)
-        TransparentUpgradeableProxy(_impl, _admin, "")
+    constructor(
+        address _impl,
+        address _admin,
+        address _ethernautAddress
+    )
+        TransparentUpgradeableProxy(
+            _impl,
+            _admin,
+            abi.encodeWithSignature("initialize(address)", _ethernautAddress)
+        )
     {}
 }
--- contracts/contracts/metrics/Stats.sol
+++ contracts/contracts/metrics/Stats.sol
@@ -1,72 +1,241 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 
-contract Stats {
+contract Statistics is Initializable {
+    address ETHERNAUT_ADDRESS;
+    bool initialized = false;
+
     struct LevelInstance {
-        address instanceAddress;
+        address instance;
         bool isCompleted;
         uint256 timeCreated;
         uint256 timeCompleted;
         uint256[] timeSubmitted;
     }
     mapping(address => mapping(address => LevelInstance)) playerStats;
-    mapping(address => bool) playerExists;
+    mapping(address => bool) public playerExists;
     address[] players;
+
     struct Level {
         uint256 noOfInstancesCreated;
         uint256 noOfInstancesSubmitted_Success;
         uint256 noOfInstancesSubmitted_Fail;
     }
     mapping(address => Level) public levelStats;
+    address[] public levels;
 
-    function saveCreateLevelStats(
-        address levelInstanceAddress,
-        address levelFactoryAddress
-    ) public {
-        if (playerExists[msg.sender] == false) {
-            players.push(msg.sender);
-            playerExists[msg.sender] = true;
+    function initialize(address _ethernautAddress) public initializer {
+        ETHERNAUT_ADDRESS = _ethernautAddress;
+    }
+
+    function createNewInstance(
+        address instance,
+        address level,
+        address player
+    ) external onlyEthernaut levelExistsCheck(level) {
+        if (playerExists[player] == false) {
+            players.push(player);
+            playerExists[player] = true;
         }
         require(
-            playerStats[msg.sender][levelFactoryAddress].instanceAddress ==
-                address(0),
-            "Level already created"
+            playerStats[player][level].instance == address(0),
+            "Instance for the level is already created"
         );
-        playerStats[msg.sender][levelFactoryAddress] = LevelInstance(
-            levelInstanceAddress,
+        playerStats[player][level] = LevelInstance(
+            instance,
             false,
             block.timestamp,
             0,
             new uint256[](0)
         );
-        levelStats[levelFactoryAddress].noOfInstancesCreated++;
+        levelStats[level].noOfInstancesCreated++;
     }
 
-    function saveSubmitLevelStats(address levelFactoryAddress, bool isCompleted)
-        public
-    {
+    function submitSuccess(
+        address instance,
+        address level,
+        address player
+    ) external onlyEthernaut levelExistsCheck(level) playerExistsCheck(player) {
         require(
-            playerStats[msg.sender][levelFactoryAddress].instanceAddress !=
-                address(0),
-            "Level not created"
+            playerStats[player][level].instance != address(0),
+            "Instance for the level is not created"
         );
         require(
-            playerStats[msg.sender][levelFactoryAddress].isCompleted == false,
+            playerStats[player][level].isCompleted == false,
             "Level already completed"
         );
 
-        playerStats[msg.sender][levelFactoryAddress].timeSubmitted.push(
-            block.timestamp
+        playerStats[player][level].timeSubmitted.push(block.timestamp);
+
+        playerStats[player][level].timeCompleted = block.timestamp;
+        playerStats[player][level].isCompleted = true;
+
+        levelStats[level].noOfInstancesSubmitted_Success++;
+    }
+
+    function submitFailure(
+        address instance,
+        address level,
+        address player
+    ) external onlyEthernaut levelExistsCheck(level) playerExistsCheck(player) {
+        require(
+            playerStats[player][level].instance != address(0),
+            "Instance for the level is not created"
+        );
+        require(
+            playerStats[player][level].isCompleted == false,
+            "Level already completed"
         );
 
-        if (isCompleted) {
-            levelStats[levelFactoryAddress].noOfInstancesSubmitted_Success++;
-            playerStats[msg.sender][levelFactoryAddress].timeCompleted = block
-                .timestamp;
-            playerStats[msg.sender][levelFactoryAddress].isCompleted = true;
-        } else {
-            levelStats[levelFactoryAddress].noOfInstancesSubmitted_Fail++;
+        playerStats[player][level].timeSubmitted.push(block.timestamp);
+
+        levelStats[level].noOfInstancesSubmitted_Fail++;
+    }
+
+    function saveNewLevel(address level) external onlyEthernaut {
+        require(
+            doesLevelExist(level) == false,
+            "Input level factory address is already added"
+        );
+        levels.push(level);
+    }
+
+    function getTotalNoOfLevelsCreatedByPlayer(address player)
+        public
+        view
+        playerExistsCheck(player)
+        returns (uint256)
+    {
+        uint256 noOfLevelsCreated = 0;
+        for (uint256 i = 0; i < levels.length; i++) {
+            if (playerStats[player][levels[i]].instance != address(0)) {
+                noOfLevelsCreated++;
+            }
         }
+        return noOfLevelsCreated;
+    }
+
+    function getTotalNoOfLevelsCompletedByPlayer(address player)
+        public
+        view
+        playerExistsCheck(player)
+        returns (uint256)
+    {
+        uint256 noOfLevelsCompleted = 0;
+        for (uint256 i = 0; i < levels.length; i++) {
+            if (playerStats[player][levels[i]].isCompleted) {
+                noOfLevelsCompleted++;
+            }
+        }
+        return noOfLevelsCompleted;
+    }
+
+    function isLevelCompleted(address player, address level)
+        public
+        view
+        playerExistsCheck(player)
+        levelExistsCheck(level)
+        returns (bool)
+    {
+        return playerStats[player][level].isCompleted;
+    }
+
+    function getTimeElapsedForCompletionOfLevel(address player, address level)
+        public
+        view
+        playerExistsCheck(player)
+        levelExistsCheck(level)
+        returns (uint256)
+    {
+        require(playerStats[player][level].isCompleted, "Level not completed");
+        return block.timestamp - playerStats[player][level].timeCompleted;
+    }
+
+    function getPercentageOfLevelsCompleted(address player)
+        public
+        view
+        playerExistsCheck(player)
+        returns (uint256)
+    {
+        return
+            (getTotalNoOfLevelsCompletedByPlayer(player) * 100) / levels.length;
+    }
+
+    function getTotalNoOfLevelsCreated() public view returns (uint256) {
+        uint256 totalNoOfLevelsCreated = 0;
+        for (uint256 i = 0; i < levels.length; i++) {
+            totalNoOfLevelsCreated += levelStats[levels[i]]
+                .noOfInstancesCreated;
+        }
+        return totalNoOfLevelsCreated;
+    }
+
+    function getTotalNoOfLevelsCompleted() public view returns (uint256) {
+        uint256 totalNoOfLevelsCompleted = 0;
+        for (uint256 i = 0; i < levels.length; i++) {
+            totalNoOfLevelsCompleted += levelStats[levels[i]]
+                .noOfInstancesSubmitted_Success;
+        }
+        return totalNoOfLevelsCompleted;
+    }
+
+    function getTotalNoOfPlayers() public view returns (uint256) {
+        return players.length;
+    }
+
+    function getNoOfFailedSubmissionForLevel(address level)
+        public
+        view
+        levelExistsCheck(level)
+        returns (uint256)
+    {
+        return levelStats[level].noOfInstancesSubmitted_Fail;
+    }
+
+    function getNoOfInstancesForLevel(address level)
+        public
+        view
+        levelExistsCheck(level)
+        returns (uint256)
+    {
+        return levelStats[level].noOfInstancesCreated;
+    }
+
+    function getNoOfCompletedSubmissionForLevel(address level)
+        public
+        view
+        levelExistsCheck(level)
+        returns (uint256)
+    {
+        return levelStats[level].noOfInstancesSubmitted_Success;
+    }
+
+    function doesLevelExist(address level) private view returns (bool) {
+        for (uint256 i = 0; i < levels.length; i++) {
+            if (levels[i] == level) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    modifier levelExistsCheck(address level) {
+        require(doesLevelExist(level), "Invalid level factory address");
+        _;
+    }
+
+    modifier playerExistsCheck(address player) {
+        require(playerExists[player], "Invalid player address");
+        _;
+    }
+
+    modifier onlyEthernaut() {
+        require(
+            msg.sender == ETHERNAUT_ADDRESS,
+            "Only Ethernaut can call this function"
+        );
+        _;
     }
 }
