b4dbc4200a4fbd3302281d9e2d50ac0dba9782c3
JithinKS <jithinks112@gmail.com>
2022-11-14 14:03:28 +0530

    added gap variable

--- contracts/contracts/metrics/Statistics.sol
+++ contracts/contracts/metrics/Statistics.sol
@@ -294,4 +294,12 @@ contract Statistics is Initializable {
     function doesPlayerExist(address player) public view returns (bool) {
         return playerExists[player];
     }
+
+    
+    /**
+     * @dev This empty reserved space is put in place to allow future versions to add new
+     * variables without shifting down storage in the inheritance chain.
+     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+     */
+    uint256[45] private __gap;
 }
