dd668dae6813a72255ea6d892a216ca5ebec2693
OjusWiZard <ojuswimail@gmail.com>
2022-12-06 16:58:56 +0530

    feat: github issue open script
    Signed-off-by: OjusWiZard <ojuswimail@gmail.com>

---
+++ client/src/utils/github.js
@@ -0,0 +1,51 @@
+import open from 'open';
+
+
+export default function newGithubIssueUrl(options = {}) {
+    let repoUrl;
+    if (options.repoUrl) {
+        repoUrl = options.repoUrl;
+    } else if (options.user && options.repo) {
+        repoUrl = `https://github.com/${options.user}/${options.repo}`;
+    } else {
+        throw new Error('You need to specify either the `repoUrl` option or both the `user` and `repo` options');
+    }
+
+    const url = new URL(`${repoUrl}/issues/new`);
+
+    const types = [
+        'body',
+        'title',
+        'labels',
+        'template',
+        'milestone',
+        'assignee',
+        'projects',
+    ];
+
+    for (const type of types) {
+        let value = options[type];
+        if (value === undefined) {
+            continue;
+        }
+        if (type === 'labels' || type === 'projects') {
+            if (!Array.isArray(value)) {
+                throw new TypeError(`The \`${type}\` option should be an array`);
+            }
+            value = value.join(',');
+        }
+        url.searchParams.set(type, value);
+    }
+    return url.toString();
+}
+
+
+export const raiseIssue = async () => {
+    const url = newGithubIssueUrl({
+        user: 'OpenZeppelin',
+        repo: 'ethernaut',
+        template: 'New-Network-Support.md'
+    });
+
+    await open(url);
+}
