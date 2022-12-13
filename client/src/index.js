ba5355fc5f56962a3de860a65cb9b4e64fe0805a
Dario <dariolobuglio@gmail.com>
2022-12-14 00:14:21 +0100

    Resolve ios (#463)
    * Merge branch 'master' into feat/enable-user-deploy
    
    * deactivate hubspot
    
    * reactivate
    
    * final fix
    
    * more expliciti
    
    * fix mobile

--- client/src/index.js
+++ client/src/index.js
@@ -19,6 +19,7 @@ import Header from "./containers/Header";
 import MarkdownComponent from "./components/Markdown";
 import { loadTranslations } from "./utils/translations";
 import Footer from "./components/Footer";
+import parse from "html-react-parser";
 
 // For bundle splitting without lazy loading.
 const nonlazy = (component) => lazy(() => component);
@@ -47,35 +48,60 @@ if (!window.web3) {
 
   root.render(
     <div>
-      <div className="lines"></div>
-      <main className="boxes">
-        <h3>Setup Metamask</h3>
-        <section>
-          <MarkdownComponent target={strings.setupMetamask} />
-        </section>
+      {/* Parent container */}
+      <main>
+          {/* Main title and buttons */}
+          <section className="titles">
+            <a href={constants.PATH_ROOT}>
+              <img
+                id="the-ethernaut"
+                src="../../imgs/the-ethernaut.svg"
+                alt="The-Ethernaut"
+                className="the-ethernaut"
+                style={{ width: "80%"}}
+              />
+            </a>
+          </section>
+          <section className="Description">
+              <center>
+                <hr />
+              </center>
+              <div style={{ width: "150%", marginLeft: "-25%" }}>{parse(strings.info)}</div>
+          </section>
+         
+         <center >
+          <div className="boxes">
 
-        <h3>Game Mechanics</h3>
-        <section>
-          <MarkdownComponent target={strings.gameMechanics} />
-        </section>
+            <h3>Setup Metamask</h3>
+            <section>
+              <MarkdownComponent target={strings.setupMetamask} />
+            </section>
 
-        <h3>Using the console</h3>
-        <section>
-          <MarkdownComponent target={strings.usingConsole} />
-        </section>
+            <h3>Game Mechanics</h3>
+            <section>
+              <MarkdownComponent target={strings.gameMechanics} />
+            </section>
 
-        <h3>Beyond the console</h3>
-        <section>
-          <MarkdownComponent target={strings.beyondConsole} />
-        </section>
+            <h3>Using the console</h3>
+            <section>
+              <MarkdownComponent target={strings.usingConsole} />
+            </section>
 
-        <h3>Troubleshooting</h3>
-        <section>
-          <MarkdownComponent target={strings.troubleshooting} />
-        </section>
+            <h3>Beyond the console</h3>
+            <section>
+              <MarkdownComponent target={strings.beyondConsole} />
+            </section>
+
+            <h3>Troubleshooting</h3>
+            <section>
+              <MarkdownComponent target={strings.troubleshooting} />
+            </section>
+            </div>
+          </center>
+         
+          {/* Footer */}
+          <Provider store={store}><Footer></Footer></Provider>
       </main>
-      {/* Footer */}
-      <Provider store={store}><Footer></Footer></Provider>
     </div>
   )
 } else {
