f1d0dd65940ee43d8c94bd63b2db9669f8920be4
xaler <dariolobuglio@gmail.com>
2023-01-03 16:30:50 +0100

    restore fixes

--- client/src/utils/getlevelsdata.js
+++ client/src/utils/getlevelsdata.js
@@ -6,11 +6,12 @@ var levels = require(`../gamedata/gamedata.json`).levels;
 
 
 export const getLevelDetailsByAddress = (levelAddress, chainId) => {
+    const constants = require("../constants");
     const allLevels = require("client/src/gamedata/gamedata.json").levels;
     // If we are on a predeployed chain, fetch address from constants
     const gamedata = onPredeployedNetwork(chainId)
-      ? require(`client/src/gamedata/deploy.${ID_TO_NETWORK[chainId]}.json`)
-      : restoreContract("77");
+    ? require(`client/src/gamedata/deploy.${constants.ID_TO_NETWORK[chainId]}.json`)
+    : restoreContract("77");
     // If we are not fetch from localstorage
     // based on the index fetch the level name and difficulty
     const addressToId = Object.fromEntries(
