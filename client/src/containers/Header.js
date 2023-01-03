c95eda91479e7bafb01329b3dce12372798a8cf0
Dario <dariolobuglio@gmail.com>
2023-01-03 10:32:04 +0100

    Revert "add changes"

--- client/src/containers/Header.js
+++ client/src/containers/Header.js
@@ -233,7 +233,7 @@ class Header extends React.Component {
                   </a>
                   <a
                     onClick={() => {
-                      this.changeLanguage("zh_cn");
+                      this.changeLanguage("cn_simplified");
                     }}
                     href="/"
                   >
@@ -246,14 +246,6 @@ class Header extends React.Component {
                     href="/"
                   >
                     {strings.french}
-                  </a>
-                  <a
-                    onClick={() => {
-                      this.changeLanguage("zh_tw");
-                    }}
-                    href="/"
-                  >
-                    {strings.chinese_traditional}
                   </a>
                   <a
                     onClick={() => {
--- client/src/gamedata/zh_tw/descriptions/levels/naughtcoin.md
+++
@@ -1,6 +0,0 @@
-NaughtCoin 是一種 ERC20 代幣，而且你已經持有這些代幣。問題是你只能在等待 10 年的鎖倉期之後才能轉移它們。你能不能嘗試將它們轉移到另一個地址，讓你可以自由地使用它們嗎？要完成這個關卡的話要讓你的帳戶餘額歸零。
-
-&nbsp;
-可能會有用的資訊
-*  [ERC20 標準](https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md)
-*  [OpenZeppelin 程式庫](https://github.com/OpenZeppelin/zeppelin-solidity/tree/master/contracts)
