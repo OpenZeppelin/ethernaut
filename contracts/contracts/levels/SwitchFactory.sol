362ee6bb744e0b9c5eafccf0e6da658127b94297
CeliktepeMurat <celiktepemurat@gmail.com>
2023-01-09 15:41:31 +0300

    feat: fix the problems in level and upgrade solidity version

--- contracts/contracts/levels/SwitchFactory.sol
+++ contracts/contracts/levels/SwitchFactory.sol
@@ -1,19 +1,29 @@
-pragma solidity ^0.4.24;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-import './base/Level.sol';
-import './Switch.sol';
+import "./base/Level.sol";
+import "./Switch.sol";
 
 contract SwitchFactory is Level {
+    function createInstance(address _player)
+        public
+        payable
+        override
+        returns (address)
+    {
+        _player;
+        Switch _switch = new Switch();
+        return address(_switch);
+    }
 
-  function createInstance(address _player) public payable returns (address) {
-    _player;
-    Switch _switch = new Switch();
-    return _switch;
-  }
-
-  function validateInstance(address _instance, address) public returns (bool) {
-    Switch _switch = Switch(_instance);
-    return _switch.switchOn();
-  }
-
+    function validateInstance(address payable _instance, address _player)
+        public
+        view
+        override
+        returns (bool)
+    {
+        _player;
+        Switch _switch = Switch(_instance);
+        return _switch.switchOn();
+    }
 }
