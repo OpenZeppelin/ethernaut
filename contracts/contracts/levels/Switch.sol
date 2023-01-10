088518373aa4d2c530f5fa7909e0d54e154a67a8
CeliktepeMurat <celiktepemurat@gmail.com>
2023-01-10 10:31:51 +0300

    upgrade contracts and fix tests

--- contracts/contracts/levels/Switch.sol
+++ contracts/contracts/levels/Switch.sol
@@ -1,35 +1,39 @@
-pragma solidity ^0.4.24;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 contract Switch {
-  bool public switchOn; // switch is off
-  bytes4 public offSelector = bytes4(keccak256("turnSwitchOff()"));
+    bool public switchOn; // switch is off
+    bytes4 public offSelector = bytes4(keccak256("turnSwitchOff()"));
 
-  function flipSwitch(bytes _data) onlyOff public
-  {
-      require(address(this).call(_data), "call failed :(");
-  }
-
-  function turnSwitchOn() onlyThis public  {
-      switchOn = true;
-  }
-
-  function turnSwitchOff() onlyThis public {
-     switchOn = false;
-  }
-
-  modifier onlyThis {
-      require(msg.sender == address(this), "Only the contract can call this");
-      _;
-  }
-
-  modifier onlyOff {
-    // we use a complex data type to put in memory
-    bytes32[1] memory selector;
-    // check that the calldata at position 68 (location of _data)
-    assembly {
-      calldatacopy(selector, 68, 4) // grab function selector from calldata
+    function flipSwitch(bytes memory _data) public onlyOff {
+        (bool success, ) = address(this).call(_data);
+        require(success, "call failed :(");
     }
-    require(selector[0] == offSelector, "Can only call the turnOffSwitch function");
-    _;
-  }
+
+    function turnSwitchOn() public onlyThis {
+        switchOn = true;
+    }
+
+    function turnSwitchOff() public onlyThis {
+        switchOn = false;
+    }
+
+    modifier onlyThis() {
+        require(msg.sender == address(this), "Only the contract can call this");
+        _;
+    }
+
+    modifier onlyOff() {
+        // we use a complex data type to put in memory
+        bytes32[1] memory selector;
+        // check that the calldata at position 68 (location of _data)
+        assembly {
+            calldatacopy(selector, 68, 4) // grab function selector from calldata
+        }
+        require(
+            selector[0] == offSelector,
+            "Can only call the turnOffSwitch function"
+        );
+        _;
+    }
 }
