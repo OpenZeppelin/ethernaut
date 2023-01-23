3e22cb83b38dfd382128ac4bde0af9c9a1a07cf8
itaylevy <itaylevy134@gmail.com>
2023-01-23 12:41:09 +0200

    Now the mocked uniswap factory contract looks awesome

--- contracts/contracts/levels/PriceItFactory.sol
+++ contracts/contracts/levels/PriceItFactory.sol
@@ -49,16 +49,21 @@ contract TestingERC20 is Ownable, ERC20 {
 }
 
 contract MockedUniswapV2Factory {
+  mapping(address => mapping(address => address)) public getPair;
+
   function createPair(address tokenA, address tokenB) external returns (address pair) {
     require(tokenA != tokenB, "UniswapV2: IDENTICAL_ADDRESSES");
     (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
     require(token0 != address(0), "UniswapV2: ZERO_ADDRESS");
+    require(getPair[token0][token1] == address(0), 'UniswapV2: PAIR_EXISTS');
     bytes memory bytecode = type(UniswapV2Pair).creationCode;
     bytes32 salt = keccak256(abi.encodePacked(token0, token1));
     assembly {
       pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
     }
     IUniswapV2Pair(pair).initialize(token0, token1);
+    getPair[token0][token1] = pair;
+    getPair[token1][token0] = pair;
     return pair;
   }
 }
