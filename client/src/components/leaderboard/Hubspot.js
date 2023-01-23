96790b42cc21a8db29a73f511d03c25d319cda27
JithinKS <jithinks112@gmail.com>
2023-01-23 17:22:35 +0530

    added color mode to forms

--- client/src/components/leaderboard/Hubspot.js
+++ client/src/components/leaderboard/Hubspot.js
@@ -18,7 +18,8 @@ class HubspotForm extends React.Component{
         window.hbspt.forms.create({
           target: "#hubspotForm",
           ...this.props,
-          onFormReady: this.onFormReady
+          onFormReady: this.onFormReady,
+          submitButtonClass: "leaderboard-alias-submit-button"
         });
       }
     });
@@ -27,22 +28,33 @@ class HubspotForm extends React.Component{
   onFormReady = (form) => { 
     const spans = form.querySelectorAll("span");
     const legends = form.querySelectorAll(".hs-field-desc");
+    const buttons = form.querySelectorAll(".leaderboard-alias-submit-button");
 
     const emailElement = spans[0];
     const usernameElement = spans[2]
     const addressElement = spans[4]
     const label = legends[1]
 
-    console.log(legends)
+    console.log(buttons)
 
     const styles = getComputedStyle(document.documentElement);
 
     const bgColor = styles.getPropertyValue('--secondary-color');
+    const textColor = styles.getPropertyValue('--primary-color');
 
     emailElement.style.color = bgColor;
     usernameElement.style.color = bgColor;
     addressElement.style.color = bgColor;
+    
     label.style.color = bgColor;
+    label.style.opacity = 0.7;
+
+    buttons[0].style.backgroundColor = bgColor;
+    buttons[0].style.border = "none";
+    buttons[0].style.color = textColor;
+    buttons[0].style.padding = '8px';
+    buttons[0].style.borderRadius = '5px';
+    buttons[0].style.cursor = 'pointer';
   }
 
   render() {
