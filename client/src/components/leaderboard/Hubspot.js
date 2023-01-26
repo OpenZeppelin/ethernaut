3966342ad84ec929af4cdf18dc68e5d8f5143e63
JithinKS <jithinks112@gmail.com>
2023-01-26 13:56:10 +0530

    added logic for adding aliases

--- client/src/components/leaderboard/Hubspot.js
+++ client/src/components/leaderboard/Hubspot.js
@@ -95,6 +95,10 @@ class HubspotForm extends React.Component{
     form.style.color = textColor;
   }
 
+  onFormSubmit = (form, data) => { 
+    console.log(data)
+  }
+
   render() {
     return (
       <>
