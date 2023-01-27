242f24ca06f9b13b1456537f5d02d5cd9868673b
JithinKS <jithinks112@gmail.com>
2023-01-27 14:30:56 +0530

    removed query selector error

--- client/src/components/leaderboard/Hubspot.js
+++ client/src/components/leaderboard/Hubspot.js
@@ -12,24 +12,17 @@ class HubspotForm extends React.Component{
   }
 
   componentDidMount() {
-    const script = document.createElement("script");
-    script.src = "https://js.hsforms.net/forms/v2.js";
-    document.body.appendChild(script);
-
-    script.addEventListener("load", () => {
+    if (window.hbspt) {
       // @ts-ignore
-      if (window.hbspt) {
-        // @ts-ignore
-        window.hbspt.forms.create({
-          target: "#hubspotForm",
-          ...this.props,
-          onFormReady: this.onFormReady,
-          submitButtonClass: "leaderboard-alias-submit-button",
-          onFormSubmitted: this.onFormSubmitted,
-          onFormSubmit:this.onFormSubmit
-        });
-      }
-    });
+      window.hbspt.forms.create({
+        target: "#hubspotForm",
+        ...this.props,
+        onFormReady: this.onFormReady,
+        submitButtonClass: "leaderboard-alias-submit-button",
+        onFormSubmitted: this.onFormSubmitted,
+        onFormSubmit:this.onFormSubmit
+      });
+    }
   }
 
   onFormReady = (form) => { 
@@ -38,10 +31,12 @@ class HubspotForm extends React.Component{
     const buttons = form.querySelectorAll(".leaderboard-alias-submit-button");
     const inputs = form.querySelectorAll("input");
 
+
     const emailElement = spans[0]
     const usernameElement = spans[2]
     const label = legends[1]
     const addressInput = inputs[2]
+    const ethernautAddressLabel = spans[4]
 
     const styles = getComputedStyle(document.documentElement);
 
@@ -50,6 +45,7 @@ class HubspotForm extends React.Component{
 
     emailElement.style.color = textColor;
     usernameElement.style.color = textColor;
+    ethernautAddressLabel.style.color = textColor;
     
     label.style.color = textColor;
     label.style.opacity = 0.7;
@@ -90,13 +86,16 @@ class HubspotForm extends React.Component{
   }
 
   onFormSubmitted = (form) => { 
+    form.remove();
     const styles = getComputedStyle(document.documentElement);
     const textColor = styles.getPropertyValue('--secondary-color');
-    form.style.color = textColor;
-  }
-
-  onFormSubmit = (form, data) => { 
-    console.log(data)
+    const outerElement = document.querySelector('#hubspotForm')
+    const element = document.createElement('div')
+    element.innerHTML = "Thanks for submitting the form."
+    element.style.color = textColor
+    element.style.position = 'relative'
+    element.style.fontSize = '17px'
+    outerElement.insertAdjacentElement('afterbegin', element)
   }
 
   render() {
