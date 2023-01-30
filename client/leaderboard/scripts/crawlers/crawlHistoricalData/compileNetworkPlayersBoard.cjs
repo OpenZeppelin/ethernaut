53d711b938bda10cdd7732d52c569bcb9ffd1e77
CeliktepeMurat <celiktepemurat@gmail.com>
2023-01-30 17:19:28 +0300

    feat: fix null values

--- client/leaderboard/scripts/crawlers/crawlHistoricalData/compileNetworkPlayersBoard.cjs
+++ client/leaderboard/scripts/crawlers/crawlHistoricalData/compileNetworkPlayersBoard.cjs
@@ -1,11 +1,18 @@
 const fs = require("fs");
-const { evaluateHistoricalPlayersProfile } = require("../../tools/evaluateHelper.cjs")
+const {
+  evaluateHistoricalPlayersProfile,
+} = require("../../tools/evaluateHelper.cjs");
 
 const compileNetworkPlayersBoard = async (networks) => {
   for (let network of networks) {
     let allPlayers = [];
-    const networkBoard = require(`../../../networks/${String(network.name).toLowerCase()}/${network.name}NetworkBoard.json`);
-    const networkBoardWithTotals = evaluateHistoricalPlayersProfile(networkBoard, network);
+    const networkBoard = require(`../../../networks/${String(
+      network.name
+    ).toLowerCase()}/${network.name}NetworkBoard.json`);
+    const networkBoardWithTotals = evaluateHistoricalPlayersProfile(
+      networkBoard,
+      network
+    );
     networkBoardWithTotals.forEach((profile) => {
       allPlayers.push(profile);
     });
@@ -14,8 +21,16 @@ const compileNetworkPlayersBoard = async (networks) => {
       `get out your lightsabre - we have morphed the ${network.name}NetworkBoard to create the ${network.name}PlayersBoard!`
     );
 
-    let networkAllPlayersPath = `client/leaderboard/networks/${String(network.name).toLowerCase()}/${network.name}PlayersBoard.json`;
-    fs.writeFileSync(networkAllPlayersPath, JSON.stringify(allPlayers), (err) => { console.log(error) });
+    let networkAllPlayersPath = `client/leaderboard/networks/${String(
+      network.name
+    ).toLowerCase()}/${network.name}PlayersBoard.json`;
+    fs.writeFileSync(
+      networkAllPlayersPath,
+      JSON.stringify(allPlayers),
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 };
 
