c213432e32b9526a1249714c5d14fbff691a0633
JithinKS <jithinks112@gmail.com>
2023-02-03 19:37:59 +0530

    pointing to update branch now

--- client/src/constants.js
+++ client/src/constants.js
@@ -192,8 +192,8 @@ export const ID_TO_NETWORK = id_to_network;
 // -----------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------
 
-export const ALIAS_PATH = "https://raw.githubusercontent.com/OpenZeppelin/ethernaut-leaderboard/main/boards/aliases.json"
+export const ALIAS_PATH = "https://raw.githubusercontent.com/OpenZeppelin/ethernaut-leaderboard/update/boards/aliases.json"
 
 export const getLeaderboardPath = (network) => {
-  return `https://raw.githubusercontent.com/OpenZeppelin/ethernaut-leaderboard/main/boards/networkleaderboards/${network}LeaderBoard.json`
+  return `https://raw.githubusercontent.com/OpenZeppelin/ethernaut-leaderboard/update/boards/networkleaderboards/${network}LeaderBoard.json`
 }
