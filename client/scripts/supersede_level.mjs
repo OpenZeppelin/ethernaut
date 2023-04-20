3daa494996a5776ee82c565f91799430396dd8fc
GianfrancoBazzani <bazzanigianfranco@gmail.com>
2023-04-20 08:54:33 +0200

    Update supersede_level.mjs

--- client/scripts/supersede_level.mjs
+++ client/scripts/supersede_level.mjs
@@ -4,14 +4,13 @@ import fs from "fs";
 import HDWalletProvider from "@truffle/hdwallet-provider";
 import Web3 from "web3";
 import * as ethutil from "../src/utils/ethutil.js";
+import * as constants from "../src/constants.js";
 import * as EthernautABI from "contracts/build/contracts/Ethernaut.sol/Ethernaut.json" assert { type: "json" };
 import * as ProxyStatsABI from "contracts/build/contracts/proxy/ProxyStats.sol/ProxyStats.json" assert { type: "json" };
 import * as ProxyAdminABI from "contracts/build/contracts/proxy/ProxyAdmin.sol/ProxyAdmin.json" assert { type: "json" };
 import * as ImplementationABI from "contracts/build/contracts/metrics/Statistics.sol/Statistics.json" assert { type: "json" };
 
-import * as constants from "../src/constants.js";
 import gamedata from "../src/gamedata/gamedata.json" assert { type: "json" };
-import { resolve } from "path";
 
 const levels = gamedata.levels;
 let web3;
@@ -33,18 +32,17 @@ let statsImplementation;
   },
  */
 // then uncomment one of the following lines to get forked network deployment data.
-const oldDeployData = await loadDeployData(
+const DeployData = await loadDeployData(
   `./client/src/gamedata/deploy.${constants.NETWORKS.GOERLI.name}.json`
 );
-// const oldDeployData = await loadDeployData(`./client/src/gamedata/deploy.${constants.NETWORKS.MUMBAI.name}.json`);
-// const oldDeployData = await loadDeployData(`./client/src/gamedata/deploy.${constants.NETWORKS.SEPOLIA.name}.json`);
-// const oldDeployData = await loadDeployData(`./client/src/gamedata/deploy.${constants.NETWORKS.OPTIMISM_GOERLI.name}.json`);
-// const oldDeployData = await loadDeployData(`./client/src/gamedata/deploy.${constants.NETWORKS.ARBITRUM_GOERLI.name}.json`);
+// const DeployData = await loadDeployData(`./client/src/gamedata/deploy.${constants.NETWORKS.MUMBAI.name}.json`);
+// const DeployData = await loadDeployData(`./client/src/gamedata/deploy.${constants.NETWORKS.SEPOLIA.name}.json`);
+// const DeployData = await loadDeployData(`./client/src/gamedata/deploy.${constants.NETWORKS.OPTIMISM_GOERLI.name}.json`);
+// const DeployData = await loadDeployData(`./client/src/gamedata/deploy.${constants.NETWORKS.ARBITRUM_GOERLI.name}.json`);
 
 // For real purposes
 //const DEPLOY_DATA_PATH = `./client/src/gamedata/deploy.${constants.ACTIVE_NETWORK.name}.json`;
-//const oldDeployData = await loadDeployData(DEPLOY_DATA_PATH);
-const newDeployData = oldDeployData;
+//const DeployData = await loadDeployData(DEPLOY_DATA_PATH);
 
 //REMEMBER TO INCLUDE CONTRACT COMPILATION IN PACKAGE.JSON SCRIPT//
 
@@ -61,26 +59,42 @@ async function supersede() {
 
   // Get operator level choice
   console.log(colors.bold.yellow("\nWhich deployId do you want to supersede?"));
-  const LevelToBeSuperseded = await getLevelToBeSuperseded();
+  const LevelToBeSuperseded = await getLevelToBeSupersededData();
 
   // Check if level is registered into ethernaut and is not already superseded
-  if (!(await isLevelRegistered(LevelToBeSuperseded))) throw "Level is not deployed";
-  if (!(await doesLevelExistsInStatistics(LevelToBeSuperseded)))
-    throw "Level is already superseded";
+  if (!(await isLevelRegistered(LevelToBeSuperseded))) {
+    console.log(colors.bold.red("Level is not deployed"));
+    process.exit();
+  }
+  if (!(await doesLevelExistsInStatistics(LevelToBeSuperseded))) {
+    console.log(colors.bold.red("Level is already superseded"));
+    process.exit();
+  }
 
   // Print level info
   await printLevelInfo(LevelToBeSuperseded);
 
   // Confirm substitution by operator
   console.log(colors.bold.yellow("\nConfirm substitution?"));
-  if (!(await operatorConfirmation(LevelToBeSuperseded)))
-    throw "Substitution not confirmed by operator";
+  if (!(await operatorConfirmation(LevelToBeSuperseded))) {
+    console.log(colors.bold.red("Substitution not confirmed by operator"));
+    process.exit();
+  }
 
   // Deploy new version
-  await deployLevel(LevelToBeSuperseded);
+  const newLevelContract = await deployLevel(LevelToBeSuperseded);
+
+  // Update deploy data object
+  const { oldAddress, newAddress } = storeSubstitutionInDeployData(
+    newLevelContract,
+    LevelToBeSuperseded
+  );
+
+  console.log(oldAddress, newAddress);
 
   // Register new address in ethernaut
-  console.log(colors.bold.yellow("Registering level in ethernaut contract..."));
+  console.log(colors.bold.yellow("\nRegistering level in ethernaut contract..."));
+
 
   // Backup
   console.log(colors.bold.yellow("Generating data backup json to allow easy recovery..."));
@@ -98,6 +112,7 @@ async function supersede() {
   console.log(
     colors.bold.yellow("Downgrading statisticsSuperseder contract to original statistics...")
   );
+  process.exit();
 }
 
 prompt.start();
@@ -105,7 +120,7 @@ await initWeb3();
 await loadGameContracts();
 await supersede();
 
-async function getLevelToBeSuperseded() {
+async function getLevelToBeSupersededData() {
   const options = {
     properties: {
       deployId: {
@@ -116,23 +131,26 @@ async function getLevelToBeSuperseded() {
   };
 
   const { deployId } = await prompt.get(options);
-  if (deployId >= levels.length || deployId < 0) throw "deployId entered must be in the list";
+  if (deployId >= levels.length || deployId < 0) {
+    console.log(colors.bold.red("deployId entered must be in the list"));
+    process.exit();
+  }
 
   return levels[deployId];
 }
 
 async function isLevelRegistered(level) {
-  const levelAddress = oldDeployData[level.deployId];
+  const levelAddress = DeployData[level.deployId];
   return await ethernaut.methods["registeredLevels(address)"](levelAddress);
 }
 
 async function doesLevelExistsInStatistics(level) {
-  const levelAddress = oldDeployData[level.deployId];
+  const levelAddress = DeployData[level.deployId];
   return await proxyStatsWithImplementationABI.methods["doesLevelExist(address)"](levelAddress);
 }
 
 async function printLevelInfo(level) {
-  const levelAddress = oldDeployData[level.deployId];
+  const levelAddress = DeployData[level.deployId];
   const FailedSubmissions = await proxyStatsWithImplementationABI.methods[
     "getNoOfFailedSubmissionsForLevel(address)"
   ](levelAddress);
@@ -152,7 +170,9 @@ async function printLevelInfo(level) {
 }
 
 async function deployLevel(level) {
-  console.log(`\n Deploying ${level.levelContract}, deployId: ${level.deployId}...`);
+  console.log(
+    colors.bold.yellow(`\nDeploying ${level.levelContract}, deployId: ${level.deployId}...`)
+  );
 
   const LevelABI = JSON.parse(
     fs.readFileSync(
@@ -171,36 +191,78 @@ async function deployLevel(level) {
   let from = constants.ADDRESSES[constants.ACTIVE_NETWORK.name];
   if (!from) from = (await web3.eth.getAccounts())[0];
 
-  //const Contract = await ethutil.getTruffleContract(LevelABI, { from });
-  //const contract = await Contract.new(...level.deployParams, props);
+  const LevelContract = await ethutil.getTruffleContract(LevelABI, { from });
+  const newLevelContract = await LevelContract.new(...level.deployParams, props);
+  await web3.eth.getTransactionReceipt(newLevelContract.transactionHash);
+  console.log(colors.grey("\n Done!"), "✅");
+  console.log(" new Address:", colors.bold.green(`${newLevelContract.address}`));
 
-
-  /// TODO FIX ERROR HERE, EXECUTIONS DO NOT FINALIZE
-
-  
-  
-  
-  // console.log(colors.grey("\n Done!"),"✅");
-  // console.log(" new Address", colors.bold.green(`${contract.address}`));
-  // oldDeployData[level.deployId] = contract.address;
-  // console.log(
-  //   colors.gray(`  storing deployed id: ${level.deployId} with address: ${contract.address}`)
-  // );
+  return newLevelContract;
 }
 
-async function operatorConfirmation() {
-  const options = {
-    properties: {
-      confirmSubstitution: {
-        pattern: new RegExp("^[yYnN]$"),
-        description: `(y/n)`,
-        message: "invalid input",
-      },
-    },
-  };
+function storeSubstitutionInDeployData(newLevelContract, level) {
+  console.log(colors.gray(` storing substitution of level with deployId: ${level.deployId}`));
+  console.log(
+    colors.gray(` ${DeployData[level.deployId]} --> ${newLevelContract.address}`)
+  );
 
-  const { confirmSubstitution } = await prompt.get(options);
-  return confirmSubstitution === "y" || confirmSubstitution === "Y";
+  if (!DeployData.supersededAddresses) {
+    DeployData.supersededAddresses = [];
+  }
+
+  const i = DeployData.supersededAddresses.push({
+    oldAddress: DeployData[level.deployId],
+    newAddress: newLevelContract.address,
+  });
+  DeployData[level.deployId] = newLevelContract.address;
+
+  return DeployData.supersededAddresses[i - 1];
+}
+
+async function loadGameContracts() {
+  let from = constants.ADDRESSES[constants.ACTIVE_NETWORK.name];
+  if (!from) from = (await web3.eth.getAccounts())[0];
+
+  // Ethernaut
+  const Ethernaut = await ethutil.getTruffleContract(EthernautABI.default, {
+    from,
+  });
+  ethernaut = await Ethernaut.at(DeployData.ethernaut);
+
+  // Statistics proxy
+  const ProxyStats = await ethutil.getTruffleContract(ProxyStatsABI.default, {
+    from,
+  });
+  proxyStats = await ProxyStats.at(DeployData.proxyStats);
+
+  // Statistics proxy with implementation ABI to call implementation functions with proxy state
+  const ProxyStatsWithImplementationABI = await ethutil.getTruffleContract(
+    ImplementationABI.default,
+    {
+      from,
+    }
+  );
+  proxyStatsWithImplementationABI = await ProxyStatsWithImplementationABI.at(DeployData.proxyStats);
+
+  // Statistics proxy admin
+  const ProxyAdmin = await ethutil.getTruffleContract(ProxyAdminABI.default, {
+    from,
+  });
+  proxyAdmin = await ProxyAdmin.at(DeployData.proxyAdmin);
+
+  // Statistics implementation
+  const StatsImplementation = await ethutil.getTruffleContract(ImplementationABI.default, {
+    from,
+  });
+  statsImplementation = await StatsImplementation.at(DeployData.implementation);
+}
+
+function loadDeployData(path) {
+  try {
+    return JSON.parse(fs.readFileSync(path, "utf8"));
+  } catch (err) {
+    return {};
+  }
 }
 
 async function initWeb3() {
@@ -218,7 +280,7 @@ async function initWeb3() {
     }
 
     web3 = new Web3(provider);
-    ethutil.setWeb3(web3);
+    //ethutil.setWeb3(web3);
 
     web3.eth.net.isListening((err, res) => {
       if (err) {
@@ -233,52 +295,19 @@ async function initWeb3() {
   });
 }
 
-async function loadGameContracts() {
-  let from = constants.ADDRESSES[constants.ACTIVE_NETWORK.name];
-  if (!from) from = (await web3.eth.getAccounts())[0];
+async function operatorConfirmation() {
+  const options = {
+    properties: {
+      confirmSubstitution: {
+        pattern: new RegExp("^[yYnN]$"),
+        description: `(y/n)`,
+        message: "invalid input",
+      },
+    },
+  };
 
-  // Ethernaut
-  const Ethernaut = await ethutil.getTruffleContract(EthernautABI.default, {
-    from,
-  });
-  ethernaut = await Ethernaut.at(oldDeployData.ethernaut);
-
-  // Statistics proxy
-  const ProxyStats = await ethutil.getTruffleContract(ProxyStatsABI.default, {
-    from,
-  });
-  proxyStats = await ProxyStats.at(oldDeployData.proxyStats);
-
-  // Statistics proxy with implementation ABI to call implementation functions with proxy state
-  const ProxyStatsWithImplementationABI = await ethutil.getTruffleContract(
-    ImplementationABI.default,
-    {
-      from,
-    }
-  );
-  proxyStatsWithImplementationABI = await ProxyStatsWithImplementationABI.at(
-    oldDeployData.proxyStats
-  );
-
-  // Statistics proxy admin
-  const ProxyAdmin = await ethutil.getTruffleContract(ProxyAdminABI.default, {
-    from,
-  });
-  proxyAdmin = await ProxyAdmin.at(oldDeployData.proxyAdmin);
-
-  // Statistics implementation
-  const StatsImplementation = await ethutil.getTruffleContract(ImplementationABI.default, {
-    from,
-  });
-  statsImplementation = await StatsImplementation.at(oldDeployData.implementation);
-}
-
-function loadDeployData(path) {
-  try {
-    return JSON.parse(fs.readFileSync(path, "utf8"));
-  } catch (err) {
-    return {};
-  }
+  const { confirmSubstitution } = await prompt.get(options);
+  return confirmSubstitution === "y" || confirmSubstitution === "Y";
 }
 
 /*
