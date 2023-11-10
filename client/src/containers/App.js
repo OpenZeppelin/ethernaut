934d776abf94a4ebf37b86b066aaf4544531f3ac
GianfrancoBazzani <bazzanigianfranco@gmail.com>
2023-11-10 11:37:32 +0100

    Add network deprecation banner

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -8,12 +8,26 @@ import parse from "html-react-parser";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { withRouter } from "../hoc/withRouter";
-import { randGoodIcon } from "../utils/^^";
-import { deployAdminContracts } from '../utils/deploycontract';
+import { randGoodIcon, randBadIcon } from "../utils/^^";
+import { deployAdminContracts } from "../utils/deploycontract";
+import {
+  networkOnDeprecationOrDeprecated,
+  isDeprecatedNetwork,
+  deprecationStatus,
+  deprecationDate
+} from "../utils/networkDeprecation";
 
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      chainId: 0,
+    };
+    if (window.ethereum) {
+      window.ethereum.request({ method: "eth_chainId" }).then((id) => {
+        this.setState({ chainId: Number(id) });
+      });
+    }
 
     // Analytics
     ReactGA.initialize(constants.GOOGLE_ANALYTICS_ID);
@@ -31,7 +45,7 @@ class App extends React.Component {
     let target = this.props.levels[0].deployedAddress;
     for (let i = 0; i < this.props.levels.length; i++) {
       const level = this.props.levels[i];
-      if (!level.deployedAddress) { 
+      if (!level.deployedAddress) {
         return this.props.navigate(`${constants.PATH_LEVEL_ROOT}${i}`);
       }
       const completed = this.props.completedLevels[level.deployedAddress];
@@ -54,39 +68,56 @@ class App extends React.Component {
 
     // change the network to Sepolia network
     async function switchToSepolia() {
-      let elements = document.querySelectorAll('.progress-bar-wrapper');
-      const deployWindow = document.querySelectorAll('.deploy-window-bg');
+      let elements = document.querySelectorAll(".progress-bar-wrapper");
+      const deployWindow = document.querySelectorAll(".deploy-window-bg");
       try {
         await window.ethereum.request({
-          method: 'wallet_switchEthereumChain',
-          params: [{ chainId: `0x${Number(constants.NETWORKS.SEPOLIA.id).toString(16)}` }],//if on wrong network giving option to switch to sepolia network.
+          method: "wallet_switchEthereumChain",
+          params: [
+            {
+              chainId: `0x${Number(constants.NETWORKS.SEPOLIA.id).toString(
+                16
+              )}`,
+            },
+          ], //if on wrong network giving option to switch to sepolia network.
         });
-        deployWindow[0].style.display = 'none';
+        deployWindow[0].style.display = "none";
       } catch (switchError) {
         // This error code indicates that the chain has not been added to MetaMask.
         if (switchError.code === 4902) {
           try {
             await window.ethereum.request({
-              method: 'wallet_addEthereumChain',
+              method: "wallet_addEthereumChain",
               params: [
                 {
-                  chainId: [{ chainId: `0x${Number(constants.NETWORKS.SEPOLIA.id).toString(16)}` }]
+                  chainId: [
+                    {
+                      chainId: `0x${Number(
+                        constants.NETWORKS.SEPOLIA.id
+                      ).toString(16)}`,
+                    },
+                  ],
                 },
               ],
             });
-            deployWindow[0].style.display = 'none';
+            deployWindow[0].style.display = "none";
           } catch (addError) {
             if (addError.code === 4001) {
               //User has rejected changing the request
-              elements[0].style.display = 'none';
+              elements[0].style.display = "none";
             }
-            console.error("Can't add nor switch to the selected network")
+            console.error("Can't add nor switch to the selected network");
           }
         } else if (switchError.code === 4001) {
           //User has rejected changing the request
-          if (elements[0]) elements[0].style.display = 'none';
+          if (elements[0]) elements[0].style.display = "none";
         }
       }
+    }
+
+    async function continueAnyway() {
+      const deployWindow = document.querySelectorAll(".deploy-window-bg");
+      deployWindow[0].style.display = "none";
     }
 
     return (
@@ -118,35 +149,49 @@ class App extends React.Component {
               </button>
             </ul>
           </section>
-          {/* Deploy Window */}
+          {/*not Deployed window*/}
           <div className="deploy-window-bg">
-            <div className="deploy-window">
-              <h1>{randGoodIcon()}</h1>
-              <h1>{strings.deployMessageTitle}</h1>
-              <br />
-              {strings.deployMessage}
-              <ul>
-                {supportedNetworks.map((network, idx) =>
-                  <li key={idx}>{network}</li>
-                )}
-              </ul>
-              <p className="deploy-note">{strings.deployConfirmation}</p>
-              <div className="choice-buttons">
-                <button
-                  className="buttons"
-                  onClick={deployAdminContracts}
-                >
-                  {strings.deployGame}
-                </button>
-                <button
-                  className="buttons"
-                  onClick={switchToSepolia}
-                >
-                  {strings.switchToSepolia}
-                </button>
+            {!networkOnDeprecationOrDeprecated(this.state.chainId) ? (
+              <div className="deploy-window">
+                {/*deploy window*/}
+                <h1>{randGoodIcon()}</h1>
+                <h1>{strings.deployMessageTitle}</h1>
+                <br />
+                {strings.deployMessage}
+                {supportedNetworksList(supportedNetworks)}
+                <p className="deploy-note">{strings.deployConfirmation}</p>
+                <div className="choice-buttons">
+                  <button className="buttons" onClick={deployAdminContracts}>
+                    {strings.deployGame}
+                  </button>
+                  <button className="buttons" onClick={switchToSepolia}>
+                    {strings.switchToSepolia}
+                  </button>
+                </div>
+                <p className="deploy-note">{strings.deployNote}</p>
               </div>
-              <p className="deploy-note">{strings.deployNote}</p>
-            </div>
+            ) : (
+              <div className="deploy-window">
+                {/*deprecation window*/}
+                <h1>{randBadIcon()}</h1>
+                <h1>
+                  {isDeprecatedNetwork(this.state.chainId)? strings.deprecatedNetwork : strings.networkBeingDeprecated}
+                </h1>
+                <br />
+                {strings.deployMessage}
+                {supportedNetworksList(supportedNetworks)}
+                <div className="choice-buttons">
+                  <button className="buttons" onClick={switchToSepolia}>
+                    {strings.switchToSepolia}
+                  </button>
+                  {!isDeprecatedNetwork(this.state.chainId) && 
+                    <button className="buttons" onClick={continueAnyway}>
+                      {strings.continueAnyway}
+                    </button>
+                  }
+                </div>
+              </div>
+            )}
           </div>
           {/* Levels */}
           <Mosaic></Mosaic>
@@ -174,6 +219,20 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({}, dispatch);
+}
+
+function supportedNetworksList(_supportedNetworks) {
+  return (
+  <ul>
+    {_supportedNetworks.map((network, idx) => (
+      <li key={idx}>
+        {network}
+        {networkOnDeprecationOrDeprecated(constants.NETWORKS[network].id) 
+          && " (" + deprecationStatus(constants.NETWORKS[network].id) + 
+          " on " + deprecationDate(constants.NETWORKS[network].id) + ")"}
+      </li>
+    ))}
+  </ul>)
 }
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
