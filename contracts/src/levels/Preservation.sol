2f44105593ad506b5ef05339421caaa7a7aaad59
leovct <l009.vincent@gmail.com>
2024-09-24 20:41:30 +0200

    fix: prevent cheating and make sure that contracts implement the library contract

--- contracts/src/levels/Preservation.sol
+++ contracts/src/levels/Preservation.sol
@@ -3,28 +3,28 @@ pragma solidity ^0.8.0;
 
 contract Preservation {
     // public library contracts
-    address public timeZone1Library;
-    address public timeZone2Library;
+    LibraryContract public timeZone1Library;
+    LibraryContract public timeZone2Library;
     address public owner;
     uint256 storedTime;
     // Sets the function signature for delegatecall
     bytes4 constant setTimeSignature = bytes4(keccak256("setTime(uint256)"));
 
-    constructor(address _timeZone1LibraryAddress, address _timeZone2LibraryAddress) {
-        timeZone1Library = _timeZone1LibraryAddress;
-        timeZone2Library = _timeZone2LibraryAddress;
+    constructor(LibraryContract _timeZone1LibraryContract, LibraryContract _timeZone2LibraryContract) {
+        timeZone1Library = _timeZone1LibraryContract;
+        timeZone2Library = _timeZone2LibraryContract;
         owner = msg.sender;
     }
 
     // set the time for timezone 1
     function setFirstTime(uint256 _timeStamp) public {
-        (bool success, ) = timeZone1Library.delegatecall(abi.encodePacked(setTimeSignature, _timeStamp));
+        (bool success, ) = address(timeZone1Library).delegatecall(abi.encodePacked(setTimeSignature, _timeStamp));
         require(success);
     }
 
     // set the time for timezone 2
     function setSecondTime(uint256 _timeStamp) public {
-        (bool success, ) = timeZone2Library.delegatecall(abi.encodePacked(setTimeSignature, _timeStamp));
+        (bool success, ) = address(timeZone2Library).delegatecall(abi.encodePacked(setTimeSignature, _timeStamp));
         require(success);
     }
 }
