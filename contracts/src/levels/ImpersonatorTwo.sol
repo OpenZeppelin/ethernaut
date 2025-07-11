2452fbd783b93f9eb00db3e7d172e2219b1353a5
jack695 <jacquotvincent96@gmail.com>
2025-07-11 12:17:14 +0200

    New level based on repeated nonces
    This level is based on the fact than any private key which reuses a nonce twice can be recovered.

---
+++ contracts/src/levels/ImpersonatorTwo.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "openzeppelin-contracts-08/access/Ownable.sol";
+import "openzeppelin-contracts-08/utils/cryptography/ECDSA.sol";
+
+using Strings for uint256;
+
+contract ImpersonatorTwo is Ownable {
+    address public admin;
+    uint256 public nonce;
+    bool locked;
+
+    constructor() payable {}
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Not admin");
+        _;
+    }
+
+    event SignatureVerification(bytes32 hash, bytes signature);
+
+    function setAdmin(bytes memory signature, address newAdmin) public {
+        string memory message = string(
+            abi.encodePacked("admin", nonce.toString())
+        );
+        require(_verify(hash_message(message), signature), "Invalid signature");
+        nonce++;
+        admin = newAdmin;
+    }
+
+    function switchLock(bytes memory signature) public {
+        string memory message = string(
+            abi.encodePacked("lock", nonce.toString())
+        );
+        require(_verify(hash_message(message), signature), "Invalid signature");
+        nonce++;
+        locked = !locked;
+    }
+
+    function withdraw() public onlyAdmin {
+        require(!locked, "Funds are locked");
+        payable(owner()).transfer(address(this).balance);
+    }
+
+    function hash_message(string memory message) public pure returns (bytes32) {
+        return ECDSA.toEthSignedMessageHash(abi.encodePacked(message));
+    }
+
+    function _verify(
+        bytes32 hash,
+        bytes memory signature
+    ) internal returns (bool) {
+        emit SignatureVerification(hash, signature);
+        return ECDSA.recover(hash, signature) == owner();
+    }
+}
