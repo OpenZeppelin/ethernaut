b489e1ff5dadb88e011745e33c69f413eddd27e1
GianfrancoBazzani <bazzanigianfranco@gmail.com>
2025-09-16 21:56:04 +0200

    🔨 fix: make constants privates

--- contracts/src/levels/ImpersonatorTwoFactory.sol
+++ contracts/src/levels/ImpersonatorTwoFactory.sol
@@ -5,12 +5,12 @@ import "./base/Level.sol";
 import "./ImpersonatorTwo.sol";
 
 contract ImpersonatorTwoFactory is Level {
-    bytes public SWITCH_LOCK_SIG = abi.encodePacked(
+    bytes constant SWITCH_LOCK_SIG = abi.encodePacked(
         hex"e5648161e95dbf2bfc687b72b745269fa906031e2108118050aba59524a23c40", // r
         hex"70026fc30e4e02a15468de57155b080f405bd5b88af05412a9c3217e028537e3", // s
         uint8(27) // v
     );
-    bytes public SET_ADMIN_SIG = abi.encodePacked(
+    bytes constant SET_ADMIN_SIG = abi.encodePacked(
         hex"e5648161e95dbf2bfc687b72b745269fa906031e2108118050aba59524a23c40", // r
         hex"4c3ac03b268ae1d2aca1201e8a936adf578a8b95a49986d54de87cd0ccb68a79", // s
         uint8(27) // v
