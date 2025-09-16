e6b559cd27df8557524778e6f331c5954aa1897d
GianfrancoBazzani <bazzanigianfranco@gmail.com>
2025-09-16 10:58:08 +0200

    :shipit: feat: deploy Cashback level

--- client/src/constants.js
+++ client/src/constants.js
@@ -196,12 +196,11 @@ export const SHOW_ALL_COMPLETE_DESCRIPTIONS = false; /* FALSE on production */
 export const SHOW_VERSION = true;
 
 // export const ACTIVE_NETWORK = NETWORKS.SEPOLIA
-// export const ACTIVE_NETWORK = NETWORKS.MUMBAI
 // export const ACTIVE_NETWORK = NETWORKS.OPTIMISM_SEPOLIA
 // export const ACTIVE_NETWORK = NETWORKS.ARBITRUM_SEPOLIA
 // export const ACTIVE_NETWORK = NETWORKS.HOLESKY
 // export const ACTIVE_NETWORK = NETWORKS.AMOY
-export const ACTIVE_NETWORK = NETWORKS.LOCAL;
+// export const ACTIVE_NETWORK = NETWORKS.LOCAL;
 
 let id_to_network = {};
 Object.keys(NETWORKS)
